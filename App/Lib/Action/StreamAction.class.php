62a20b94927e176be9310fb2c0e32cd44417c65a
wangdx <echowdx@gmail.com>
2013-07-21 10:43:02 +0800

    add redis message queue _fix bug

--- App/Lib/Action/StreamAction.class.php
+++ App/Lib/Action/StreamAction.class.php
@@ -883,13 +883,14 @@ class StreamAction extends Action {
         		/**
         		 * record diffusion limit nums 5 per 12 hours
         		 */
-        		$time12hour = time() - 43200;
-        		$userDiffusion12hourAll = $RecordDiffusion->where("uid = $userloginid AND time > $time12hour")->order("time DESC")->count();
-        		if ($userDiffusion12hourAll >= 3) {
-        			echo "你扩散了太多消息，休息休息再来吧 :)";
-        			echo "<br />每12小时最多扩散3条";
-        			exit();
-        		}
+                //TODO for test temporarily
+//        		$time12hour = time() - 43200;
+//        		$userDiffusion12hourAll = $RecordDiffusion->where("uid = $userloginid AND time > $time12hour")->order("time DESC")->count();
+//        		if ($userDiffusion12hourAll >= 3) {
+//        			echo "你扩散了太多消息，休息休息再来吧 :)";
+//        			echo "<br />每12小时最多扩散3条";
+//        			exit();
+//        		}
         		
         		/**
         		 * insert diffusion record
