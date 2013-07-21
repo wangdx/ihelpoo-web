be8558352f30efe86baf40596ee332c0a5e080e7
chenkehao <121670155@qq.com>
2013-07-21 11:21:45 +0800

    test shell add

--- App/Lib/Action/RooterAction.class.php
+++ App/Lib/Action/RooterAction.class.php
@@ -331,7 +331,7 @@ class RooterAction extends Action {
     public function schoolinfo()
     {
     	$admin = logincheck();
-    	$this->assign('title','开通学校');
+    	$this->assign('title','a开通学校');
     	$SchoolInfo = M("SchoolInfo");
     	$SchoolInfo->select();
     	exit('dd');
