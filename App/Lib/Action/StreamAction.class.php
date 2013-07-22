e752dbbf7bbf71c75c84539c649fbb7e73c37b52
wangdx <echowdx@gmail.com>
2013-07-22 17:52:19 +0800

    add id worker

--- App/Lib/Action/StreamAction.class.php
+++ App/Lib/Action/StreamAction.class.php
@@ -963,8 +963,8 @@ class StreamAction extends Action {
 
 
                 Vendor('Ihelpoo.Idworker');
-                $idworker = new Idworker(1);
-                $a = $idworker->nextId();
+                $idworker = new Idworker().generateParticle(1);
+//                $a = $idworker;
 //                $Idwork = new Idwork(2);
 //                $b = $idworker->nextId();
 
@@ -976,7 +976,7 @@ class StreamAction extends Action {
         	    $userPriorityNums = sizeof($userPriorityObj);
 
 
-       	        echo $a."-".$b."已经扩散给了 <a href='".__ROOT__."/mutual/priority?me'>你的圈子</a> 中的等<span class='f14 fb orange'>".$userPriorityNums."</span> 人...<br /><br />";
+       	        echo $idworker."已经扩散给了 <a href='".__ROOT__."/mutual/priority?me'>你的圈子</a> 中的等<span class='f14 fb orange'>".$userPriorityNums."</span> 人...<br /><br />";
        	        if (!empty($userPriorityNums)) {
        	            $i = 0;
         	        foreach ($userPriorityObj as $userPriority) {
--- ThinkPHP/Extend/Vendor/Ihelpoo/Idworker.php
+++ ThinkPHP/Extend/Vendor/Ihelpoo/Idworker.php
@@ -1,90 +1,39 @@
 <?php
 class Idworker
-{
-    const debug = 0;
-    static $workerId;
-    static $twepoch = 1361775855078;
-    static $sequence = 0;
-    const workerIdBits = 4;
-    static $maxWorkerId = 15;
-    const sequenceBits = 10;
-    static $workerIdShift = 10;
-    static $timestampLeftShift = 14;
-    static $sequenceMask = 1023;
-    private static $lastTimestamp = -1;
+{	const EPOCH=1364036124000;
 
-    function __construct($workId)
+
+
+    final public function generateParticle($machine_id)
     {
-        if ($workId > self::$maxWorkerId || $workId < 0) {
-            throw new Exception("worker Id can't be greater than 15 or less than 0");
-        }
-        self::$workerId = $workId;
+//Time - 42 bits (millisecond precision w/ a custom epoch gives us 96 years 1 month 21 days 16 hours 42 minutes 24 seconds in the future)
+        $time = floor(microtime(true) * 1000);
 
-//        echo 'logdebug->__construct()->self::$workerId:' . self::$workerId;
-//        echo '</br>';
+//Substract custom epoch from current time
+        $time -= time()*1000;
 
+//Add to base
+        $base = pow(2,41);
+        $base += $time;
+        $base = decbin($base);
+
+//configured machine id - 10 bits - to 1024 machines
+        $machineid = decbin($machine_id);
+
+//sequence number - 12 bits - up to 4096 random numbers per machine
+        $random = mt_rand(0,pow(2,12)-1);
+        $random = decbin($random);
+
+//Pack
+        $base = $base.$machineid.$random;
+
+        return base_convert($base,2,10);
     }
 
-    function timeGen()
+    final public function timeFromParticle($particle)
     {
-//获得当前时间戳
-        $time = explode(' ', microtime());
-        $time2 = substr($time[0], 2, 3);
-        $timestramp = $time[1] . $time2;
-//        echo 'logdebug->timeGen()->$timestramp:' . $time[1] . $time2;
-//        echo '</br>';
-        return $time[1] . $time2;
+        return base_convert(substr(base_convert($particle,10,2),0,42),2,10)-pow(2,41)+self::EPOCH;
     }
-
-    function  tilNextMillis($lastTimestamp)
-    {
-        $timestamp = $this->timeGen();
-        while ($timestamp <= $lastTimestamp) {
-            $timestamp = $this->timeGen();
-        }
-
-//        echo 'logdebug->tilNextMillis()->$timestamp:' . $timestamp;
-//        echo '</br>';
-        return $timestamp;
-    }
-
-    function  nextId()
-    {
-        $timestamp = $this->timeGen();
-//        echo 'logdebug->nextId()->self::$lastTimestamp1:' . self::$lastTimestamp;
-//        echo '</br>';
-        if (self::$lastTimestamp == $timestamp) {
-            self::$sequence = (self::$sequence + 1) & self::$sequenceMask;
-            if (self::$sequence == 0) {
-//                echo "###########" . self::$sequenceMask;
-                $timestamp = $this->tilNextMillis(self::$lastTimestamp);
-//                echo 'logdebug->nextId()->self::$lastTimestamp2:' . self::$lastTimestamp;
-//                echo '</br>';
-            }
-        } else {
-            self::$sequence = 0;
-//            echo 'logdebug->nextId()->self::$sequence:' . self::$sequence;
-//            echo '</br>';
-        }
-        if ($timestamp < self::$lastTimestamp) {
-            throw new Excwption("Clock moved backwards.  Refusing to generate id for " . (self::$lastTimestamp - $timestamp) . " milliseconds");
-        }
-        self::$lastTimestamp = $timestamp;
-//        echo 'logdebug->nextId()->self::$lastTimestamp3:' . self::$lastTimestamp;
-//        echo '</br>';
-//
-//        echo 'logdebug->nextId()->(($timestamp - self::$twepoch << self::$timestampLeftShift )):' . ((sprintf('%.0f', $timestamp) - sprintf('%.0f', self::$twepoch)));
-//        echo '</br>';
-        $nextId = ((sprintf('%.0f', $timestamp) - sprintf('%.0f', self::$twepoch))) | (self::$workerId << self::$workerIdShift) | self::$sequence;
-//        echo 'timestamp:' . $timestamp . '-----';
-//        echo 'twepoch:' . sprintf('%.0f', self::$twepoch) . '-----';
-//        echo 'timestampLeftShift =' . self::$timestampLeftShift . '-----';
-//        echo 'nextId:' . $nextId . '----';
-//        echo 'workId:' . self::$workerId . '-----';
-//        echo 'workerIdShift:' . self::$workerIdShift . '-----';
-        return $nextId;
-    }
-
 }
 
 
