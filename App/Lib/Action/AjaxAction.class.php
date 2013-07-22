7ef049c6ce2dcee901369b38192095af79a25ced
wangdx <echowdx@gmail.com>
2013-07-22 22:44:18 +0800

    add id worker

--- App/Lib/Action/AjaxAction.class.php
+++ App/Lib/Action/AjaxAction.class.php
@@ -32,7 +32,7 @@ class AjaxAction extends Action {
             $redismq->connect('127.0.0.1', 6379);
     		$messageAtNums = $MsgAt->where("touid = $userloginid AND deliver = 0")->count();
     		$messageCommentNums = $MsgComment->where("uid = $userloginid AND deliver = 0")->count();
-    		$messageSystemNums = $persons = $redismq->hGet(C('I_MSG').C('I_MSG_SYSTEM'), $userloginid);//$MsgSystem->where("uid = $userloginid AND deliver = 0")->count();
+    		$messageSystemNums = $redismq->hGet(C('I_MSG').C('I_MSG_SYSTEM'), $userloginid);//$MsgSystem->where("uid = $userloginid AND deliver = 0")->count();
     		$messageTalkNums = $TalkContent->where("touid = $userloginid AND deliver = 0")->count();
     		if (!empty($messageTalkNums)) {
     			$lastTalkContent = $TalkContent->where("touid = $userloginid AND deliver = 0")
