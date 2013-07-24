4a17b5a85ce7fb00185a634946b91ba33ab06d6f
wangdx <echowdx@gmail.com>
2013-07-24 22:46:13 +0800

    disabled RW_SEPARATE

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -65,7 +65,7 @@ return  array(
     'DB_FIELDS_CACHE'       => true,        // 启用字段缓存
     'DB_CHARSET'            => 'utf8',      // 数据库编码默认采用utf8
     'DB_DEPLOY_TYPE'        => 1, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-    'DB_RW_SEPARATE'        => true,       // 数据库读写是否分离 主从式有效
+    'DB_RW_SEPARATE'        => false,       // 数据库读写是否分离 主从式有效
     'DB_MASTER_NUM'         => 1, // 读写分离后 主服务器数量
     'DB_SLAVE_NO'           => '', // 指定从服务器序号
     'DB_SQL_BUILD_CACHE'    => false, // 数据库查询的SQL创建缓存
