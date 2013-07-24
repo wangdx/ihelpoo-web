d40a6f38a83562c151b80267add9eb698cda95ea
chenkehao <121670155@qq.com>
2013-07-24 15:34:13 +0800

    test shell add

--- App/Lib/Action/StreamAction.class.php
+++ App/Lib/Action/StreamAction.class.php
@@ -548,6 +548,7 @@ class StreamAction extends Action {
         ->join('i_user_info ON i_record_say.uid = i_user_info.uid')
         ->join('i_op_specialty ON i_user_info.specialty_op = i_op_specialty.id')
         ->join('i_school_info ON i_user_login.school = i_school_info.id')
+        ->join("i_user_remark ON i_record_say.uid = i_user_remark.ruid AND $userloginid = i_user_remark.uid")
 		->field('sid,i_user_login.uid,say_type,content,image,url,i_user_login.school,comment_co,diffusion_co,hit_co,i_record_say.time,from,last_comment_ti,school_id,nickname,sex,birthday,enteryear,type,online,active,icon_url,i_user_info.specialty_op,i_op_specialty.name,i_op_specialty.academy,i_school_info.id,i_school_info.school as schoolname,i_school_info.domain,i_school_info.domain_main')
 		->limit($offset,$count)->select();
 		$userRecordSayUidBefore = NULL;
