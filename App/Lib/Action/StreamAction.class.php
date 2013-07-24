651a8bd2613cf622843b31a0c7fe42e8006a3ec7
wangdx <echowdx@gmail.com>
2013-07-24 18:18:20 +0800

    use handlersocket to insert system notice

--- App/Lib/Action/StreamAction.class.php
+++ App/Lib/Action/StreamAction.class.php
@@ -889,12 +889,12 @@ class StreamAction extends Action {
         		$diffusionId = $RecordDiffusion->add($dataDiffusion);
 
                 $hs = new HandlerSocket(C('MYSQL_MASTER'), C('HS_PORT_WR'));
-                if (!($hs->openIndex(3, C('OO_DBNAME'), 'i_msg_notice', '', 'from_id,type_id,content_id, template_id, create_time')))
+                if (!($hs->openIndex(3, C('OO_DBNAME'), 'i_msg_notice', '', 'id, from_id,type_id,content_id, template_id, create_time')))
                 {
                     echo $hs->getError(), PHP_EOL;
                     die();
                 }
-                if ($hs->executeInsert(3, array($userloginid, 1, 2, 3, time())) === false)
+                if ($hs->executeInsert(3, array('', $userloginid, 1, 2, 3, time())) === false)
                 {
                     echo $hs->getError(), PHP_EOL;
                 }
