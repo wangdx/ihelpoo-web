3237471db1540e14870781b3c1e9bc45b1d28db6
wangdx <echowdx@gmail.com>
2013-07-25 23:56:26 +0800

    try to begin to refactor code with extracting methods

--- App/Lib/Action/MessageAction.class.php
+++ App/Lib/Action/MessageAction.class.php
@@ -88,11 +88,11 @@ class MessageAction extends Action
             $from_user = "<a href='" . __ROOT__ . "/wo/" . $notice['source_id'] . "' target='_blank' class='getuserinfo' userid='" . $notice['source_id'] . "'>" . $fromUser['nickname'] . "</a>";
 
             $tpl =   $redis->hGet(C('R_Notice_Message_Template'), $notice['format_id']);
-            printf("%s 扩散了你的这条消息 <a href='%s/%s' class='%s'> 详情</a>", $from_user, __ROOT__, $redis->hGet(C('R_Notice_Message_Link'), $notice['notice_type']), $notice['detail_id']);
+            $content = sprintf($tpl, $from_user, __ROOT__, $redis->hGet(C('R_Notice_Message_Link'), $notice['notice_type']), $notice['detail_id']);
 
             $msgSysArray[] = array(
                 'deliver' => $redis->hGet(C('R_ACCOUNT'). C('R_MESSAGE') . $userloginid , $notice['notice_id']),
-                'content' => $tpl,
+                'content' => $content,
                 'time' => i_time($notice['time']),
             );
         }
