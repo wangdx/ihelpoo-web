29b0c3ca0cbd6e62d14793d384edb9d9239a36ed
wangdx <echowdx@gmail.com>
2013-07-26 22:30:49 +0800

    add plus feature

--- Public/js/stream/index.js
+++ Public/js/stream/index.js
@@ -488,7 +488,7 @@ $().ready(function(){
 //                console.log($(this).parent().find('.plus_count_region').val());
 //                $(this).parent().find('.plus_count_region').text(data);
 
-                alert($('#plus_count_region_'+$(this).attr('value')).val());
+                alert($('#plus_count_region_'+$(this).attr('value')).text());
                 $('#plus_count_region_'+$(this).attr('value')).html('<span class="red_l">'+data+'</span>');
             }
         });
