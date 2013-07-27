caed514c3892bdc726addb7f155054faa0f7b219
chenkehao <121670155@qq.com>
2013-07-27 10:55:19 +0800

    test shell add

--- App/Lib/Action/SchooladminAction.class.php
+++ App/Lib/Action/SchooladminAction.class.php
@@ -931,7 +931,7 @@ class SchooladminAction extends Action {
         	if (!empty($resultRecordSay[sid])) {
         		$this->assign('recordDelete', $resultRecordSay);
         	} else {
-        		redirect('/schooladmin/record', 3, '没有找到相关记录或者你没有其他学校删除权限...');
+        		redirect('/schooladmin/record', 3, '没有找到相关记录或者你没有其他学校记录的删除权限...');
         	}
         	
         	/**
