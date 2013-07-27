747ad48c656c6ccbf03aa3e7ba47d83eee3ef24f
wangdx <echowdx@gmail.com>
2013-07-28 00:27:37 +0800

    add group feature

---
+++ Public/js/setting/group.js
@@ -0,0 +1,29 @@
+$().ready(function(){
+    var $infoLoading = $('<img/>').attr({'src': baseUrl + 'Public/image/common/progressbar.gif', 'title': '加载中...请稍等'});
+    $("#pssubmit").click(function(){
+        var groupName = $("#group_name").val();
+        var groupDesc = $("#group_desc").val();
+        $(this).ajaxStart(function(){
+    	    $('#ajaxprogressbar').html($infoLoading);
+        }).ajaxStop(function(){
+    	    $infoLoading.remove();
+        });
+        if (groupName == "") {
+            showWrongInfo('请输入分组名');
+        } else {
+            $.post(baseUrl + "setting/group", $("#psform").serialize(), function(data){
+                if (data.status == "yes") {
+                    $("#ajaxprogressbar").html("<p id='infopsupdateok'><span class='icon_right'></span> 创建分组成功</p>");
+                    $("#infopsupdateok").slideDown('normal').delay(1000);
+                    $("#infopsupdateok").fadeOut('slow');
+                } else if (data.status == "wrong") {
+                    showWrongInfo(data.info);
+                }
+            }, "json");
+        }
+    });
+});
+function showWrongInfo(info) {
+	$("#ajaxprogressbar").html("<p id='infopsupdateok'><span class='icon_wrong'></span> " + info + "</p>");
+	$("#infopsupdateok").slideDown('normal').delay(1000).slideUp('fast');
+}
