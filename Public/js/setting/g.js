96bba2ea13bcaac28eb1c547b7a84abfe0ef8b99
wangdx <echowdx@gmail.com>
2013-07-28 12:57:34 +0800

    add group feature

--- Public/js/setting/g.js
+++ Public/js/setting/g.js
@@ -13,7 +13,7 @@ $().ready(function(){
                   } else if (data.status == "wrong") {
                       showWrongInfo(data.info);
                   }
-//                  setTimeout(function(){location.reload();},2000);
+                  setTimeout(function(){location.href=baseUrl + "setting/group";},2000);
               }, "json");
           }
 
@@ -37,7 +37,7 @@ $().ready(function(){
                 } else if (data.status == "wrong") {
                     showWrongInfo(data.info);
                 }
-//                setTimeout(function(){location.reload();},2000);
+                setTimeout(function(){location.href=baseUrl + "setting/group";},2000);
             }, "json");
         }
     });
