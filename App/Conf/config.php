d9de5c7891e711c8d4791eef31d0343e4b36f4e3
chenkehao <121670155@qq.com>
2013-07-29 11:03:33 +0800

    test shell add

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -4,9 +4,9 @@ $ooconfig = require 'config_oo.php';
 
 $config = array(
     //'配置项'=>'配置值'
-    'APP_DEBUG'=>false,
-    'SHOW_PAGE_TRACE'=>false,
-	'URL_CASE_INSENSITIVE'=>false,
+    'APP_DEBUG'=>true,
+    'SHOW_PAGE_TRACE'=>true,
+	'URL_CASE_INSENSITIVE'=>true,
     'URL_HTML_SUFFIX'=>'.html',
     'DB_PREFIX'=>'i_',
 //	'TMPL_EXCEPTION_FILE'=>'./App/Tpl/Public/error.html',
