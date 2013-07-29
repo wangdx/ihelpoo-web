ffc60a3457763ad55971e5f12b9bdf2009d7c6f6
chenkehao <121670155@qq.com>
2013-07-29 11:53:02 +0800

    test shell add

--- Public/js/index/index.js
+++ Public/js/index/index.js
@@ -21,7 +21,7 @@ $().ready(function(){
 		bgwidth = windowwidth;
 	}
 	$('#lay_bg').css({ width: bgwidth, height: bgheight});
-	$('#lay_bg_img').css({ width: bgwidth, height: bgheight}).fadeIn('slow');
+	$('#lay_bg_img').fadeIn('slow').css({ width: bgwidth, height: bgheight});
 	$('.texture_background').css({ width: bgwidth, height: bgheight});
 	/**$('#lay_bg_img').fadeIn('slow',function(){
 		loadBackground();
