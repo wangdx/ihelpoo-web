80314a34954f88a29d677adb2cdc2c60cf079431
chenkehao <121670155@qq.com>
2013-08-01 21:07:42 +0800

    test shell add

--- Public/js/wo/wo.js
+++ Public/js/wo/wo.js
@@ -1,7 +1,7 @@
 $().ready(function(){
     showtime();
     secondShine('#secondpass');
-    var iconpositionwidth = $('.icon_timebar_in_div').width() - 30;
+    var iconpositionwidth = $('.icon_timebar_in_div').width() - 35;
     $('#icon_timebar_img').animate({left: iconpositionwidth}, 1000);
 });
 function showtime(){
