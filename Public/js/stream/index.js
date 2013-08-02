aee4863059302a5cbd59d4c57715c580c939c530
chenkehao <121670155@qq.com>
2013-08-02 17:05:28 +0800

    test shell add

--- Public/js/stream/index.js
+++ Public/js/stream/index.js
@@ -523,9 +523,23 @@ $().ready(function(){
     var imageTempContent = '';
     $('.s_li_p_content_image img').live('click', function(){
     	var totalImageNums = $(this).parent().find("img").size();
-    	alert(totalImageNums);
     	if (totalImageNums > 1) {
-    		alert(totalImageNums);
+    		alert($("img").get(0));
+    		alert($(this).get());
+    		var enlargeSwitch = $(this).attr('enlargeswitch');
+	    	if (enlargeSwitch != 'on') {
+		    	var imageurl = $(this).attr('src');
+		    	if (imageurl.match("ihelpoo-public") != '') {
+		    		var reg = new RegExp("thumb_","g");
+		    		var imageurllarge = imageurl.replace(reg,"");
+		    	}
+		    	imageTempContent = $(this).parent().html();
+		    	$(this).parent().html('<p class="f12 s_li_p_content_image_title"><a href="'+imageurllarge+'" target="_blank"><span class="icon_plus"></span>查看原图</a> <a class="s_li_p_content_image_title_up"><span class="icon_up"></span>收起</a></p><img src="'+imageurllarge+'" width="395" enlargeswitch="on" title="点击缩小" /></p>');
+	    	} else {
+	    		$(this).parent().html(imageTempContent);
+	    	}
+    		
+    		
     	} else {
 	    	var enlargeSwitch = $(this).attr('enlargeswitch');
 	    	if (enlargeSwitch != 'on') {
