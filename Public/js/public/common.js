09885dd9e157b02f765ef052a3970e35f879ebb8
chenkehao <121670155@qq.com>
2013-08-02 15:39:11 +0800

    test shell add

--- Public/js/public/common.js
+++ Public/js/public/common.js
@@ -14,7 +14,7 @@ $().ready(function(){
     $("#header_nav_user").hover(
     	function(){
     		$("#nav_hover_list_div").css({left : mainpositionleft}).slideDown("fast");
-    		$("#nav_hover_list_div_ul").html("<li><a>同学</a> | <a>校园组织</a> | <a>周边商家</a></li>");
+    		$("#nav_hover_list_div_ul").html("<li><a href='/index/mate'>同学</a> | <a href='/index/group'>校园组织</a> | <a href='/index/business'>周边商家</a></li>");
     	},
       	function(){
     		$("#nav_hover_list_div").hover(function(){
@@ -25,6 +25,11 @@ $().ready(function(){
     			$("#nav_hover_list_div").slideUp("fast");
     		});
 	});
+    $(".nav_hover_up").hover(
+    	function(){
+    		$("#nav_hover_list_div").slideUp("fast");
+    	},function(){}
+    );
 
     /**
      * skin part
