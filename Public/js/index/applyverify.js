ee78661cce7fac486a30c3c784652e83b4ae1356
chenkehao <121670155@qq.com>
2013-08-02 21:40:53 +0800

    test shell add

---
+++ Public/js/index/applyverify.js
@@ -0,0 +1,25 @@
+$().ready(function(){
+    var $infoLoading = $('<img/>').attr({'src': baseUrl + 'Public/image/common/ajax_wait.gif', 'title': '提交中...请稍等'});
+    $("#submit").click(function(){
+        var applyverifyName = $("#applyverify_name").val();
+        var applyverifyMobile = $("#applyverify_mobile").val();
+        $(this).ajaxStart(function(){
+    	    $("#submit_info").html($infoLoading);
+        }).ajaxStop(function(){
+    	    $infoLoading.remove();
+        });
+        if (applyverifyName == "") {
+        	$("#submit_info").html('姓名不能为空');
+        } else if (applyverifyMobile == "") {
+        	$("#submit_info").html('手机不能为空');
+        } else {
+            $.post(baseUrl + "index/applyverify", $("#applyverifyform").serialize(), function(data){
+                if (data.status == "yes") {
+                    $("#submit_info").html("<span class='icon_right'></span> 提交成功");
+                } else if (data.status == "wrong") {
+                	$("#submit_info").html(data.info);
+                }
+            }, "json");
+        }
+    });
+});
