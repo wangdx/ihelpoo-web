9f5c3a775d03c1de1bea1d4fea7df871635e0a26
chenkehao <121670155@qq.com>
2013-08-03 08:39:48 +0800

    test shell add

--- Public/js/public/common.js
+++ Public/js/public/common.js
@@ -48,6 +48,10 @@ $().ready(function(){
     		$("#nav_hover_list_div").slideUp("fast");
     	},function(){}
     );
+    $(".header").mouseout(function(){
+    	$("#header_nav_more").removeClass('header_left_arrow_hover');
+		$("#nav_hover_list_div").slideUp("fast");
+    });
     
     
 
