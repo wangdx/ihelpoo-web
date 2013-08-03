0d88ce7f3e3c8b2883b41c081ce38ddd73cbddfb
wangdx <echowdx@gmail.com>
2013-08-03 15:20:32 +0800

    refactor chat feature

--- Public/js/talk/_to.js
+++ Public/js/talk/_to.js
@@ -1,9 +1,18 @@
-$(function(){
+$(function () {
     "use strict";
 
     var socket = $.atmosphere;
     var chatSocket;
     var transport = 'websocket';
+
+    /**
+     * send messgae
+     */
+    $('#send_message').click(function () {
+        var newContent = $('#send_message_textarea').val();
+        chatSocket.push({data: 'message=' + newContent});
+        return false;
+    });
 
 
     // We are now ready to cut the request
@@ -20,36 +29,47 @@ $(function(){
     };
 
 
-    request.onOpen = function(response) {
-        console.log("onOpen");
+    request.onMessage = function (response) {
+        console.log("onmessage");
+
+        var msg = response.responseBody;
+
+        var htmlIn = " <span class='f14 gray '>" + msg.data.nickname + "</span>"
+            + " <span class='f12 gray'>" + msg.data.time + "</span><br />"
+            + msg.data.content + "<br /><br />";
+
+        $('#show_message_div').append(htmlIn);
+        var boxHeight = $('#show_message_div').height();
+        $('#show_message_div_outer').animate({scrollTop: boxHeight}, 800);
+        $('#send_message_textarea').val('');
+        $('#image_upload_url').val('');
+        $('#image_upload_list_ul').empty();
+        $('.img_upload_comment_form_div').slideUp('fast');
     };
 
 
-    request.onMessage = function (response) {
-        console.log("onmessage");
+    request.onOpen = function (response) {
+        if (console) {
+            console.log("[INFO]: opening to talk");
+        }
+        transport = response.transport;
+    };
+
+
+    request.onReopen = function (response) {
+        if (console) {
+            console.log("[INFO]: reopen to talk");
+        }
+    };
+
+    request.onTransportFailure = function (errorMsg, request) {
+        if (window.EventSource) {
+            request.fallbackTransport = "sse";
+        }
     };
 
     chatSocket = socket.subscribe(request);
 
-
-
-//    chatSocket = $.atmosphere.subscribe("http://comet.ihelpoo.com/c1/chat/10000-12419", globalCallback, $.atmosphere.request = {
-//        logLevel: 'debug',
-//        transport: 'websocket',
-//        fallbackTransport: 'long-polling',
-//        callback: call,
-//        enableXDR: true,
-//        dropAtmosphereHeaders: true,
-//        readResponsesHeaders: false,
-//        attachHeadersAsQueryString: true
-//    });
-
-    function globalCallback(response) {
-        console.log("++++");
-        if (response.state != "messageReceived") {
-            return;
-        }
-    }
 
     function call(response) {
         console.log("Call to callbackJob2");
@@ -72,15 +92,6 @@ $(function(){
         }
         return null;
     }
-
-    /**
-     * send messgae
-     */
-    $('#send_message').click(function () {
-        chatSocket.push({data: 'message=hello, world'});
-        console.log(chatSocket);
-        return false;
-    });
 
 
 });
