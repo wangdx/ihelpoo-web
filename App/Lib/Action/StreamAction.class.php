2f628afb542552daea98d70b1c3c0a7ac6ece2be
chenkehao <121670155@qq.com>
2013-08-04 11:18:37 +0800

    test shell add

--- App/Lib/Action/StreamAction.class.php
+++ App/Lib/Action/StreamAction.class.php
@@ -811,7 +811,7 @@ class StreamAction extends Action
 		    	echo '<span class="c_v_d_b_ul_li_content_reply">';
 		    	if ($comment['uid'] == $userloginid || $comment['record_owneruid'] == $userloginid) {
 				    echo '<input type="hidden" class="reply_delete_cid" name="delcomment" value="'.$comment['cid'].'" />';
-				    echo '<a class="c_v_d_b_ul_li_content_del" value="'.$comment['cid'].'">删除</a> <i class="icon_plus"></i> ';
+				    echo '<a class="c_v_d_b_ul_li_content_del gray" value="'.$comment['cid'].'">删除</a> <i class="icon_plus"></i> ';
 			    }
 		    	if (!empty($comment['uid']) && $comment['uid'] != $userloginid) {
 			    	echo '<a class="">回复</a>';
