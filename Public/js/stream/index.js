a9e2a28226011a3af6622a3e76789df5d63ae585
chenkehao <121670155@qq.com>
2013-08-04 21:45:58 +0800

    test shell add

--- Public/js/stream/index.js
+++ Public/js/stream/index.js
@@ -348,9 +348,9 @@ $().ready(function(){
 		+ '<a class="emotionbox_change_page" title="微博" value="2">微博</a>'
 		+ '<a class="emotionbox_change_page" title="兔斯基" value="3">兔斯基</a>'
 		+ '<span class="replyemotionbox_close close_x" title="关闭">×</span>'
-		+ '</p><ul class="emotionbox_show_ul"></ul>';
+		+ '</p><ul class="emotionbox_show_ul_inner"></ul>';
         $(".replyemotionbox").fadeIn("fast").css({ position: "absolute", left: positionleft, top: positiontop }).html(emotionboxhtml);
-        $(".emotionbox_show_ul").load(baseUrl + "other/loademotion");
+        $(".emotionbox_show_ul_inner").load(baseUrl + "other/loademotion");
         return false;
     });
     $(".comment_textareaicon_replyinner").live('click', function(e){
@@ -364,7 +364,7 @@ $().ready(function(){
 		+ '<span class="replyemotionbox_close close_x" title="关闭">×</span>'
 		+ '</p><ul class="emotionbox_show_ul_inner"></ul>';
         $(".replyemotionbox").fadeIn("fast").css({ position: "absolute", left: positionleft, top: positiontop }).html(emotionboxhtml);
-        $(".emotionbox_show_ul").load(baseUrl + "other/loademotion");
+        $(".emotionbox_show_ul_inner").load(baseUrl + "other/loademotion");
         return false;
     });
     $(".replyemotionbox_close").live('click', function(){
