a1f8b42e8a954a794dc87b65e836f21a489f1eeb
chenkehao <121670155@qq.com>
2013-08-05 14:37:00 +0800

    test shell add

--- Public/js/public/common.js
+++ Public/js/public/common.js
@@ -113,8 +113,85 @@ $().ready(function(){
     	var searchWords = $("#inputSearchBox").val();
     	window.location = baseUrl + "mutual/find?username=" + searchWords;
     });
-	
+    
+    /**
+     * get userinfo
+     */
+    var t;
+    $(".getuserinfo").live('mouseenter', function(e){
+    	$this = $(this);
+    	t=setTimeout(function(){
+    		var userid = $this.attr('userid');
+    		var usernickname = $this.text();
+    		var positionleft = e.pageX + 10;
+        	var positiontop = e.pageY + 10;
+    		$.ajax({
+                type: "POST",
+        		dataType: "json",
+        		url: baseUrl + "ajax/getuserinfo",
+        		data:{userid: userid, usernickname: usernickname},
+        		success:function(msg){
+                	if (!msg.status) {
+                		var inhtml = "<span class='red_l'>"+ msg.info + "</span>";
+                		$('.user_info_div').css({ position: "absolute", left: positionleft, top: positiontop }).fadeIn('fast').html(inhtml);
+    				} else {
+    					if (msg.data.sex == 1) {
+    						if (msg.data.user_relation == 'priority') {
+        						var relationhtml = "<a href='"+baseUrl+"mutual/priority/"+msg.data.uid+"?del' target='_blank' class='btn_quaned' title='取消圈他'>已圈他</a>";
+        					} else if (msg.data.user_relation == 'shield') {
+        						var relationhtml = "<a href='"+baseUrl+"stream/u/"+msg.data.uid+" class='red_l' title='取消屏蔽' target='_blank'>已经屏蔽了他</a>";
+        					} else {
+        						var relationhtml = "<a href='"+baseUrl+"mutual/priority/"+msg.data.uid+"' target='_blank' class='btn_quan'>圈他</a>";
+        					}
+    					} else {
+    						if (msg.data.user_relation == 'priority') {
+        						var relationhtml = "<span href='"+baseUrl+"mutual/priority/"+msg.data.uid+"?del' class='btn_quaned' title='取消圈她'>已圈她</span>";
+        					} else if (msg.data.user_relation == 'shield') {
+        						var relationhtml = "<a href='"+baseUrl+"stream/u/"+msg.data.uid+" class='red_l' title='取消屏蔽' target='_blank'>已经屏蔽了她</a>";
+        					} else {
+        						var relationhtml = "<a href='"+baseUrl+"mutual/priority/"+msg.data.uid+"' target='_blank' class='btn_quan'>圈她</a>";
+        					}
+    					}
+    					if (msg.data.remark != null) {
+    						var userremarkhtml = "<a class='f12 black_l' id='user_remark_set' title='点击修改备注'>"+msg.data.remark+"</a>";
+    					} else {
+    						var userremarkhtml = "<a class='f12 black_l' id='user_remark_set' title='点击设置备注'>备注</a>";
+    					}
+    					var inhtml = "<div class='user_info_top_div' userid='"+msg.data.uid+"'>"
+    					+ "		  <a class='user_info_top_div_img_a' href='"+baseUrl+"stream/u/"+msg.data.uid+"' target='_blank'>"
+    					+ "		    <img width='60' height='45' src='"+msg.data.icon_url+"' />"
+    					+ "		    <span class='online"+msg.data.online+"'></span></a>"
+    					+ "		  <p class='user_info_top_div_nickname_p'><a href='"+baseUrl+"wo/"+msg.data.uid+"' class='f14 fb' target='_blank'>"+msg.data.nickname+"</a> "+userremarkhtml+" <span class='gray'>("+msg.data.type+")</span> <span class='level"+msg.data.degree+"'></span></p>"
+    					+ "       <p class='user_info_top_div_quan_p black_l'>圈的:<span class='fb f14'>"+msg.data.follow+"</span> 圈子:<span class='fb f14'>"+msg.data.fans+"</span> "+msg.data.constellation+"<span class='sex"+msg.data.sex+"'></span> "+relationhtml+"</p>"
+    					+ "		</div>"
+    					+ "		<div class='user_info_main_div'>"
+    					+ "			<ul>";
+    					if (msg.data.schoolname != null) {
+    						inhtml +=  "<li>学院: <a target='_blank' href='"+msg.data.domain+"index/mate?w=academy&n="+msg.data.academy_id+"'>"+msg.data.academy+"</a> (<a target='_blank' href='"+msg.data.domain+"'>"+msg.data.schoolname+"</a>)</li>";
+    					} else {
+    						inhtml +=  "<li>学院: <a target='_blank' href='"+msg.data.domain+"index/mate?w=academy&n="+msg.data.academy_id+"'>"+msg.data.academy+"</a></li>";
+    					}
+    					inhtml +=  "<li>专业: <a target='_blank' href='"+msg.data.domain+"index/mate?w=academy&n="+msg.data.academy_id+"&specialty="+msg.data.specialty_id+"'>"+msg.data.specialty+"</a></li>"
+    					+ "             <li>寝室: <a target='_blank' href='"+msg.data.domain+"index/mate?w=dormitory&n="+msg.data.dormitory_id+"'>"+msg.data.dormitory+"</a></li>"
+    					+ "             <li>"+msg.data.introduction+"</li>"
+    					+ "			</ul>"
+    					+ "		</div>";
+    					$('.user_info_div').css({ position: "absolute", left: positionleft, top: positiontop }).fadeIn('fast').html(inhtml);
+    					return false;
+    				}
+                }
+            });
+    	},1000);
+    }).mouseleave(function(){
+    	clearTimeout(t);
+    	$('.user_info_div').hover(function(){},
+    	function(){
+    		$(this).fadeOut("slow");
+    	});
+    });
+    
 });
+
 function getStringLength(str) {
 	var totalLength = 0;
 	var list = str.split("");
