fbf530490a577b8a82bf5e195e5904769f3a570d
chenkehao <121670155@qq.com>
2013-08-06 10:34:24 +0800

    test shell add

--- Public/js/public/common.js
+++ Public/js/public/common.js
@@ -12,7 +12,12 @@ $().ready(function(){
     $("#header_nav_user").hover(
     	function(){
     		var mainoffset = $('.main').offset();
-    	    var mainpositionleft = mainoffset.left + 173;
+    		if (mainoffset != null) {
+    			var mainpositionleft = mainoffset.left + 173;
+    		} else {
+    			var mainhelpoffset = $('.main_help').offset();
+    			var mainpositionleft = mainhelpoffset.left + 173;
+    		}
     		$("#nav_hover_list_div").css({left : mainpositionleft}).slideDown("fast");
     		$("#nav_hover_list_div_ul").html("<li><a href='/index/mate'>同学</a> | <a href='/index/group'>校园组织</a> | <a href='/index/business'>周边商家</a></li>");
     	},
