fd2b596dfc16fc31c0a098dec25ba20952bda91e
chenkehao <121670155@qq.com>
2013-08-06 09:26:33 +0800

    test shell add

--- Public/js/item/help.js
+++ Public/js/item/help.js
@@ -323,55 +323,6 @@ $().ready(function(){
         $('#i_shine_hit').html('<p id="i_shine_hit_in"></p>');
     });
 
-    /**
-     * get userinfo
-     */
-    var t;
-    $(".getuserinfo").mouseenter(function(e){
-    	$this = $(this);
-    	t=setTimeout(function(){
-    		var userid = $this.attr('userid');
-    		var usernickname = $this.text();
-    		var positionleft = e.pageX + 10;
-        	var positiontop = e.pageY + 10;
-    		$.ajax({
-                type: "POST",
-        		dataType: "json",
-        		url: baseUrl + "ajax/getuserinfo",
-        		data:{userid: userid, usernickname: usernickname},
-        		success:function(msg){
-                	if (!msg.status) {
-                		var inhtml = "<span class='red_l'>"+ msg.info + "</span>";
-                		$('.user_info_div').css({ position: "absolute", left: positionleft, top: positiontop }).fadeIn('fast').html(inhtml);
-    				} else {
-    					var inhtml = "<div class='user_info_top_div'>"
-    						+ "		  <a class='user_info_top_div_img_a' href='"+baseUrl+"stream/u/"+msg.data.uid+"' target='_blank'>"
-    						+ "		    <img width='60' height='45' src='"+msg.data.icon_url+"' />"
-    						+ "		    <span class='online"+msg.data.online+"'></span></a>"
-    						+ "		  <p class='user_info_top_div_nickname_p'><a href='"+baseUrl+"wo/"+msg.data.uid+"' class='f14 fb' target='_blank'>"+msg.data.nickname+"</a> <span class='gray'>("+msg.data.type+")</span> <span class='level"+msg.data.degree+"'></span></p>"
-    						+ "       <p class='user_info_top_div_quan_p black_l'>圈的:<span class='fb f14'>"+msg.data.follow+"</span> 圈子:<span class='fb f14'>"+msg.data.fans+"</span> "+msg.data.constellation+"<span class='sex"+msg.data.sex+"'></span></p>"
-    						+ "		</div>"
-    						+ "		<div class='user_info_main_div'>"
-    						+ "			<ul>"
-    						+ "             <li>学院: <a target='_blank' href='"+baseUrl+"index/mate?w=academy&n="+msg.data.academy_id+"'>"+msg.data.academy+"</a></li>"
-    						+ "             <li>专业: <a target='_blank' href='"+baseUrl+"index/mate?w=academy&n="+msg.data.academy_id+"&specialty="+msg.data.specialty_id+"'>"+msg.data.specialty+"</a></li>"
-    						+ "             <li>寝室: <a target='_blank' href='"+baseUrl+"index/mate?w=dormitory&n="+msg.data.dormitory_id+"'>"+msg.data.dormitory+"</a></li>"
-    						+ "             <li>"+msg.data.introduction+"</li>"
-    						+ "			</ul>"
-    						+ "		</div>"
-    					$('.user_info_div').css({ position: "absolute", left: positionleft, top: positiontop }).fadeIn('fast').html(inhtml);
-    					return false;
-    				}
-                }
-            });
-    	},1000);
-    }).mouseleave(function(){
-    	clearTimeout(t);
-    	$('.user_info_div').hover(function(){},
-    	function(){
-    		$(this).fadeOut("slow");
-    	});
-    });
 });
 function pageToStream(){
     window.location = baseUrl + 'stream';
