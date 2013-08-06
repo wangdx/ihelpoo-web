3b32b56cd4b92d183cad6e31f903d8291bb2334e
wangdx <echowdx@gmail.com>
2013-08-06 23:09:39 +0800

    add chat comet

--- Public/js/talk/_to.js
+++ Public/js/talk/_to.js
@@ -12,7 +12,7 @@ $(function () {
 
 
     var noActionInterval = 15; // seconds
-    $("#send_message_textarea").bind('keyup', function () {
+    $("#send_message_textarea").bind('keypress', function () {
         typing();
 
         var typingStuff = $("#send_message_textarea").val();
