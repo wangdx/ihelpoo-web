cddd13d3277fefd31fa1e12c2e3ea6abb546bd10
chenkehao <121670155@qq.com>
2013-08-06 13:46:14 +0800

    test shell add

--- App/Lib/Action/UserAction.class.php
+++ App/Lib/Action/UserAction.class.php
@@ -594,7 +594,7 @@ class UserAction extends Action {
         if (!empty($_POST['getschoollist'])) {
         	$SchoolInfo = M("SchoolInfo");
         	$resultsSchoolInfo = $SchoolInfo->select();
-        	echo '<div class="setting_school_list_div"><a class="gray f12" id="setting_school_close_span"><span class="icon_quit"></span></a><ul>';
+        	echo '<div class="setting_school_list_div"><a class="gray f12" id="setting_school_close_span"><span class="close_x" title="关闭">×</span></a><ul>';
         	foreach ($resultsSchoolInfo as $schoolInfo) {
         		echo "<li><a href='".__ROOT__."/user/register?school=$schoolInfo[id]'>$schoolInfo[school]</a></li>";
         	}
