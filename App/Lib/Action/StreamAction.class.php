8436f1a1dc85ea169d69286499523e78955ab8fa
wangdx <echowdx@gmail.com>
2013-08-14 15:25:37 +0800

    add mailgun

--- App/Lib/Action/StreamAction.class.php
+++ App/Lib/Action/StreamAction.class.php
@@ -862,6 +862,8 @@ class StreamAction extends Action
 
     public function plusToggle(){
 
+        i_sendmail('echowdx@gmail.com','我帮圈圈','有新学校申请开通我帮圈圈:)');
+
     	if(empty($_POST['plusSid'])){
     		exit();
     	}
