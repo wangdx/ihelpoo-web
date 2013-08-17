04fa4e77adb67e1469d806c0b539bf08e57ddd5f
chenkehao <121670155@qq.com>
2013-08-17 13:06:51 +0800

    test shell add

--- Public/js/item/help.js
+++ Public/js/item/help.js
@@ -304,7 +304,7 @@ $().ready(function(){
 
     //del help reply
     $('.delete_help_reply_btn').click(function(){
-        var deleteReplyValue = $('.delete_help_reply_value').val();
+        var deleteReplyValue = $(this).attr('value');
     	var infohtml = "<p>确定删除追问？</p> <a class='btn_sure' id='del_record_helpreply_btn_yes' value='"+deleteReplyValue+"'>确定</a><a class='btn_cancel'>取消</a>";
     	$alreadyDeleteHelpreplyLi = $(this).parent().parent().parent();
     	$alreadyDeleteHelpreplyLi.css("backgroundColor", "#FFFA85");
