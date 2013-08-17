828841750c7977f9623009f0ed64e66d020042b0
wangdx <echowdx@gmail.com>
2013-08-17 18:12:02 +0800

    system notice

--- Public/js/stream/index.js
+++ Public/js/stream/index.js
@@ -696,7 +696,8 @@ $().ready(function(){
             data: {'diffusionSid':diffusionSid, 'diffusionView':$diffusion_view},
             dataType: "json",
             success:function(result){
-            	var infohtml = "<p align='left'>" + result.data + result.info + "</p> <a class='btn_cancel'>确定</a>";
+                console.log(result.data);
+            	var infohtml = "<p align='left'>" + result.info + "</p> <a class='btn_cancel'>确定</a>";
             	ajaxInfo(infohtml);
                 if (result.info != '你已经扩散了这条信息') {
                 	$diffusionRecordObj.append('<span class="red_l">+1</span>');
