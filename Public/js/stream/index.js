765dfed7ffc78ea8b3499d29a77cab8625b8cb86
wangdx <echowdx@gmail.com>
2013-08-17 17:42:59 +0800

    system notice

--- Public/js/stream/index.js
+++ Public/js/stream/index.js
@@ -696,7 +696,7 @@ $().ready(function(){
 //            data: {'diffusionSid':diffusionSid, 'diffusionView':$diffusion_view},
 //            datatype: "json",
 //            success:function(msg){
-                console.log(msg.data);
+//                console.log(msg.data);
 //            	var infohtml = "<p align='left'>" + msg + "</p> <a class='btn_cancel'>确定</a>";
 //            	ajaxInfo(infohtml);
 //                if (data != '你已经扩散了这条信息') {
