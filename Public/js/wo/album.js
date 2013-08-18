68e7195315d2b70f9ee56979f6be5159356fbad9
chenkehao <121670155@qq.com>
2013-08-18 22:27:03 +0800

    test shell add

--- Public/js/wo/album.js
+++ Public/js/wo/album.js
@@ -13,21 +13,14 @@ $().ready(function(){
     /**
      * delete image
      */
-    $('#delete_image').click(function(){
-        $("#i_shine_hit_in").fadeIn('fast').html("<span id='del_image_btn_yes'>确定</span> <span id='del_image_btn_no'>取消</span>");
-    });
-    $('#del_image_btn_no').live('click', function(){
-        $("#i_shine_hit_in").slideUp('fast');
+    $('#delete_image').live('click', function(){
+    	var deleteid = $(this).attr('imageid');
+    	var infohtml = "<p>确定删除？</p> <a class='btn_sure' id='delete_image' value='"+deleteid+"'>确定</a><a class='btn_cancel'>取消</a>";
+    	ajaxInfo(infohtml);
     });
     $('#del_image_btn_yes').live('click', function(){
-    	var $infoLoading = $('<img/>').attr({'src': baseUrl + 'Public/image/common/ajax_wait.gif', 'title': '提交中...请稍等'});
-    	var delImageid = $('#delete_image').attr('imageid');
-    	$(this).ajaxStart(function(){
-    		$('#del_image_btn_yes').html($infoLoading);
-        }).ajaxStop(function(){
-    	    $infoLoading.remove();
-    	    $('#del_image_btn_yes').html('确定');
-        });
+    	var delImageid = $(this).attr("value");
+    	$('#del_image_btn_yes').html($infoLoading);
     	$.ajax({
     		type: "POST",
     		url: baseUrl + "wo/album",
@@ -43,25 +36,31 @@ $().ready(function(){
     				} else if (msg.data.say_type == '1') {
     					var $htmlIn = "<div><span class='icon_pump'></span><a href='"+baseUrl+"item/help/"+msg.data.sid+"' target='_blank' class='delete_more_info'>详情</a></div>";
     				}
-    				$('#i_shine_hit_in').fadeIn('normal').html(msg.info + $htmlIn);
+    				ajaxInfo(msg.info + $htmlIn);
      			} else if (msg.status == 'existcomment') {
      				var $htmlIn = "<div><span class='icon_pump'></span><a href='"+baseUrl+"item/say/"+msg.data.sid+"' target='_blank' class='delete_more_info'>详情</a></div>";
-     				$('#i_shine_hit_in').fadeIn('normal').html(msg.info + $htmlIn);
+     				ajaxInfo(msg.info + $htmlIn);
      			} else if (msg.status == 'existhelpreply') {
     				var $htmlIn = "<div><span class='icon_pump'></span><a href='"+baseUrl+"item/help/"+msg.data.sid+"' target='_blank' class='delete_more_info'>详情</a></div>";
-     				$('#i_shine_hit_in').fadeIn('normal').html(msg.info + $htmlIn);
+     				ajaxInfo(msg.info + $htmlIn);
      			} else if (msg.status == 'existcommodity') {
     				var $htmlIn = "<div><span class='icon_pump'></span><a href='"+baseUrl+"mall/item/"+msg.data.cid+"' target='_blank' class='delete_more_info'>详情</a></div>";
-     				$('#i_shine_hit_in').fadeIn('normal').html(msg.info + $htmlIn);
+     				ajaxInfo(msg.info + $htmlIn);
     			} else {
-    				$('#i_shine_hit_in').fadeIn('normal').html(msg.info).delay(800).fadeOut('normal');
+    				ajaxInfo(msg.info);
     			}
+    			$('#del_image_btn_yes').html('确定');
             }
         });
     });
     $('.delete_more_info').live('click', function(){
     	$("#i_shine_hit_in").fadeOut('fast');
     });
+    $('.btn_cancel').live('click', function(){
+    	$("#ajax_info_div").fadeOut("fast");
+		$("#ajax_info_div_outer").hide();
+    });
+    
 });
 function pageToWoAlbum(){
     window.location = baseUrl + 'wo/album';
