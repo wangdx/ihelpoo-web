85d00ca84a1d53b68c2ba99d0a35385af966ebac
chenkehao <121670155@qq.com>
2013-08-19 15:47:08 +0800

    test shell add

--- App/Lib/Action/LabAction.class.php
+++ App/Lib/Action/LabAction.class.php
@@ -35,7 +35,7 @@ class LabAction extends Action {
     	$title = "在线区域位置4.1 ".$recordSchoolInfo['school']." 帮助主题社交网站";
         $this->assign('title',$title);
         $UserLogin = M("UserLogin");
-        $userOnlineObject = $UserLogin->where("online != 0 AND school = $recordSchoolInfo[id]")
+        $userOnlineObject = $UserLogin->where("online != 0 AND i_user_login.school = $recordSchoolInfo[id]")
         ->join('i_user_info ON i_user_info.uid = i_user_login.uid')
         ->join('i_user_status ON i_user_status.uid = i_user_login.uid')
         ->join('i_op_dormitory ON i_op_dormitory.id = i_user_info.dormitory_op')
