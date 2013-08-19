87af32110b7b98cfd345266d52e46687b936b61d
chenkehao <121670155@qq.com>
2013-08-19 17:56:05 +0800

    test shell add

--- Public/js/lab/maponline.js
+++ Public/js/lab/maponline.js
@@ -1,10 +1,10 @@
 $().ready(function(){
     $('.user_show_p').hover(
         function(){
-            $(this).animate({ top: "-=30" }, 500);
+            $(this).animate({ top: "-=10" }, 500);
         },
         function(){
-            $(this).animate({ top: "+=30" }, 500);
+            $(this).animate({ top: "+=10" }, 500);
         }
     );
 });
