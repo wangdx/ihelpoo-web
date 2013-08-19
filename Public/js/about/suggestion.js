2635c0ccf98e371f19c60b856f7403d97e4e06ec
chenkehao <121670155@qq.com>
2013-08-19 20:48:27 +0800

    test shell add

---
+++ Public/js/about/suggestion.js
@@ -0,0 +1,22 @@
+$().ready(function(){
+    var $infoLoading = $('<img/>').attr({'src': baseUrl + 'Public/image/common/ajax_wait.gif', 'title': '提交中...请稍等'});
+    $("#submit").click(function(){
+        var connection = $("#connection").val();
+        var content = $("#content").val();
+    	 $("#submit_info").html($infoLoading);
+    	 if (connection == "") {
+         	$("#submit_info").html('联系方式不能为空哦');
+         } else if (content == "") {
+         	$("#submit_info").html('意见建议还没填呢');
+         } else {
+            $.post(baseUrl + "about/snsapply", $("#applyverifyform").serialize(), function(data){
+                if (data.status == "yes") {
+                    $("#submit_info").html("<span class='icon_right'></span> 提交成功");
+                    window.location = baseUrl + 'about/suggestion?succ=ok';
+                } else if (data.status == "wrong") {
+                	$("#submit_info").html(data.info);
+                }
+            }, "json");
+        }
+    });
+});
