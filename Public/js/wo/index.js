9a98d4633aee510bc15dbea2621aab29944ad303
chenkehao <121670155@qq.com>
2013-08-19 11:03:51 +0800

    test shell add

--- Public/js/wo/index.js
+++ Public/js/wo/index.js
@@ -275,7 +275,7 @@ $().ready(function(){
     });
     
     $('.btn_cancel').live('click', function(){
-    	$("#ajax_info_div").fadeOut("fast");
+    	$("#ajax_info_div").hide();
 		$("#ajax_info_div_outer").hide();
 		$deleteCommentLi.css("backgroundColor", "#FFF");
     });
