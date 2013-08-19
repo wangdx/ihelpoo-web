beea202747cc49fa55e478e0349417aa515f9d19
chenkehao <121670155@qq.com>
2013-08-19 17:54:49 +0800

    test shell add

--- Public/js/lab/maponline.js
+++ Public/js/lab/maponline.js
@@ -1,11 +1,9 @@
 $().ready(function(){
     $('.user_show_p').hover(
         function(){
-            $(this).css({zIndex: '1112'});
             $(this).animate({ top: "-=30" }, 500);
         },
         function(){
-            $(this).css({zIndex: '999'});
             $(this).animate({ top: "+=30" }, 500);
         }
     );
