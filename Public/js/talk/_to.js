ce29138cc8948a61f21ee20ba63c8399b4a98695
wangdx <echowdx@gmail.com>
2013-08-21 00:25:51 +0800

    combine notice and chat service

--- Public/js/talk/_to.js
+++ Public/js/talk/_to.js
@@ -296,19 +296,36 @@ function Chat(state) {
         console.log(curTo + " " + from);
         if (curTo == from) {
             return;
-        } else if (chat == '4') {
-            var num = $('#message_system_nums_a').data(from);
-            num = num ? $('#message_system_nums_a').data(from) : 0;
-            $('#message_system_nums_a').data(from, num + 1)
-            $('#message_system_nums_a').show();
-            $('#message_system_nums_a').children('span').html('+' + (num + 1));
-        } else if (chat == '1') {
-            $('#message_talk_nums_div').fadeIn('fast');
-            $('#message_talk_nums_img_icon').show().attr({'src': 'http://ihelpoo.b0.upaiyun.com/useralbum/' + from + '/' + imageThumb + '_m.jpg', 'title': fromUser});
-            $('#message_talk_nums_img_icon').error(function(){$(this).hide();}).attr({'src': '/Public/image/common/0.jpg', 'title': fromUser});
-            $('#message_talk_nums_span_content').html(' ' + image);
-            $('#message_talk_nums_p_content_info').html('来自' + fromUser + '的悄悄话');
-            $('.message_talk_to_url').attr({ href: baseUrl + "talk/to/" + from });
+        } else{
+            if (chat == '4') {
+                if(from == 'at'){
+                    var num = $('#message_at_nums_a').data(from);
+                    num = num ? $('#message_at_nums_a').data(from) : 0;
+                    $('#message_at_nums_a').data(from, num + 1)
+                    $('#message_at_nums_a').show();
+                    $('#message_at_nums_a').children('span').html('+' + (num + 1));
+                }else  if(from == 'comment'){
+                    var num = $('#message_comment_nums_a').data(from);
+                    num = num ? $('#message_comment_nums_a').data(from) : 0;
+                    $('#message_comment_nums_a').data(from, num + 1)
+                    $('#message_comment_nums_a').show();
+                    $('#message_comment_nums_a').children('span').html('+' + (num + 1));
+
+                }else{
+                    var num = $('#message_system_nums_a').data(from);
+                    num = num ? $('#message_system_nums_a').data(from) : 0;
+                    $('#message_system_nums_a').data(from, num + 1)
+                    $('#message_system_nums_a').show();
+                    $('#message_system_nums_a').children('span').html('+' + (num + 1));
+                }
+            } else if (chat == '1') {
+                $('#message_talk_nums_div').fadeIn('fast');
+                $('#message_talk_nums_img_icon').show().attr({'src': 'http://ihelpoo.b0.upaiyun.com/useralbum/' + from + '/' + imageThumb + '_m.jpg', 'title': fromUser});
+                $('#message_talk_nums_img_icon').error(function(){$(this).hide();}).attr({'src': '/Public/image/common/0.jpg', 'title': fromUser});
+                $('#message_talk_nums_span_content').html(' ' + image);
+                $('#message_talk_nums_p_content_info').html('来自' + fromUser + '的悄悄话');
+                $('.message_talk_to_url').attr({ href: baseUrl + "talk/to/" + from });
+            }
         }
     };
 
