7f4494a88f661a62025857f5bb7cf5671a064daa
wangdx <echowdx@gmail.com>
2013-08-22 20:10:22 +0800

    navigate to chat page, should display the emotion rather than character

--- App/Lib/Action/TalkAction.class.php
+++ App/Lib/Action/TalkAction.class.php
@@ -187,6 +187,7 @@ class TalkAction extends Action
         $leaveWords = $TalkContent->where("i_talk_content.uid = $toUserId AND touid = $userloginid AND deliver = '0' ")
             ->join('i_user_login ON i_talk_content.uid = i_user_login.uid')
             ->order("time ASC")->select();
+
         $this->assign('leaveWords', $leaveWords);
         foreach ($leaveWords as $leaveWord) {
             $updateData = array(
