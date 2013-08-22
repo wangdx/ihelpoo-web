ca34cbd6ba2298fdc0e386976c77696d0d27ef15
wangdx <echowdx@gmail.com>
2013-08-22 20:20:56 +0800

    navigate to chat page, should display the emotion rather than character

--- Public/js/stream/notice.js
+++ Public/js/stream/notice.js
@@ -117,7 +117,8 @@ function Notice(state) {
                 $('#message_system_nums_a').children('span').html('+' + (num + 1));
             }
         } else if (chat == '1') {
-            $('#message_talk_nums_div').fadeIn('fast');
+
+            $('#message_talk_nums_div').show().fadeIn('fast');
             $('#message_talk_nums_img_icon').show().attr({'src': 'http://ihelpoo.b0.upaiyun.com/useralbum/'+from+'/'+imageThumb+'_m.jpg', 'title': fromUser})
                 .error(function(){$(this).unbind("error").attr("src", "/Public/image/common/0.jpg");});
             $('#message_talk_nums_span_content').html(' ' + image);
