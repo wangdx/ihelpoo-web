d1a9b1ee0c9fc4bba16dd2466478663418cbdce8
chenkehao <121670155@qq.com>
2013-08-22 10:48:08 +0800

    test shell add

--- App/Lib/Action/UserAction.class.php
+++ App/Lib/Action/UserAction.class.php
@@ -764,7 +764,7 @@ class UserAction extends Action {
 	            	);
 	            	$UserInfo->save($newUserInfoPrioritiedData);
 	            }
-	            redirect('/', 3, '注册成功啦...3秒后跳转到登录页面');
+	            redirect('/index', 3, '注册成功啦...3秒后跳转到登录页面');
 	        }
         }
         $this->display();
