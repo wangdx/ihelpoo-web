d24ed07b663b57b7979bafd12f8a137dcff1281c
chenkehao <121670155@qq.com>
2013-08-25 12:00:16 +0800

    test shell add

--- App/Lib/Action/UpdateAction.class.php
+++ App/Lib/Action/UpdateAction.class.php
@@ -19,10 +19,11 @@ class UpdateAction extends Action {
     		$total = $datacontentArray['total'];
     		$count = $datacontentArray['count'];
     		$page = $datacontentArray['page'];
-    		$handlednums = $page * $count;
+    		$handlednums = ($page + 1) * $count;
+    		$page++;
     		echo "总记录：".$total."，已处理：".$handlednums.", 当前页：".$page."...";
     		/**while ($handlednums < $total) {
-    			redirect('/update/index?p='.$page, 1, "总记录：$total，已处理：$handlednums, 当前页：$page...");
+    			redirect('/update/index?p='.$page, 1, "总记录：".$total."，已处理：".$handlednums.", 当前页：".$page."...");
     			
     			 * redirect
     			
