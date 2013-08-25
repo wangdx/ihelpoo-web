6a16ed931ce76ebfa4b47ac7c31343082eff5295
chenkehao <121670155@qq.com>
2013-08-25 12:04:17 +0800

    test shell add

--- App/Lib/Action/UpdateAction.class.php
+++ App/Lib/Action/UpdateAction.class.php
@@ -15,7 +15,6 @@ class UpdateAction extends Action {
     	$url = "http://www.ihelpoo.com/updateversion4/index?p=".$page;
     	$datacontents = file_get_contents($url);
     	$datacontentArray = json_decode($datacontents,TRUE);
-    	var_dump($datacontentArray);
     	if (is_array($datacontentArray)) {
     		$total = $datacontentArray['total'];
     		$count = $datacontentArray['count'];
