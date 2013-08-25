031b62d8b29cf85da0b5a2c1c59d4de3674a2e61
chenkehao <121670155@qq.com>
2013-08-25 21:53:42 +0800

    test shell add

--- App/Lib/Action/TestAction.class.php
+++ App/Lib/Action/TestAction.class.php
@@ -7,6 +7,7 @@ class TestAction extends Action {
 
     protected function _initialize() {
         header("Content-Type:text/html; charset=utf-8");
+        exit();
     }
     
     public function index() {
