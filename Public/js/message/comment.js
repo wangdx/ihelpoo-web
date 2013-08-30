da089b55e0342d7e9b434e02d694e3d68224487e
chenkehao <121670155@qq.com>
2013-08-30 17:52:21 +0800

    test shell add

--- Public/js/message/comment.js
+++ Public/js/message/comment.js
@@ -66,4 +66,40 @@ $().ready(function(){
 	        });
         }
     });
+    
+    /**
+     * icon reply part 
+     */
+    $(".comment_textareaicon_replyinner").live('click', function(e){
+        var positionleft = e.pageX + 10;
+    	var positiontop = e.pageY + 10;
+    	$replytextarea = $(this).parent().find('.textarea_style');
+        var emotionboxhtml = '<p class="emotionbox_close_p">'
+		+ '<a class="emotionbox_change_page bg_emotionbox_page_select" title="基本表情" value="1">基本表情</a>'
+		+ '<a class="emotionbox_change_page" title="微博" value="2">微博</a>'
+		+ '<a class="emotionbox_change_page" title="兔斯基" value="3">兔斯基</a>'
+		+ '<span class="replyemotionbox_close close_x" title="关闭">×</span>'
+		+ '</p><ul class="emotionbox_show_ul_inner"></ul>';
+        $(".replyemotionbox").fadeIn("fast").css({ position: "absolute", left: positionleft, top: positiontop }).html(emotionboxhtml);
+        $(".emotionbox_show_ul_inner").load(baseUrl + "other/loademotion");
+        return false;
+    });
+    $(".replyemotionbox_close").live('click', function(){
+    	 $(".replyemotionbox").slideUp('fast');
+    	 return false;
+    });
+    $('.emotionbox_show_ul_inner img').live("click", function(){
+        var imgtitle = $(this).attr('title');
+        var imgtitlemarkin = '[' + imgtitle + ']';
+        var textareanow = $replytextarea.val() + imgtitlemarkin;
+        $replytextarea.val(textareanow);
+        $(".replyemotionbox").fadeOut('fast');
+        return false;
+    });
+    $('.emotionbox_change_page').live('click', function(){
+    	$(".emotionbox_change_page").removeClass('bg_emotionbox_page_select');
+    	$(this).addClass('bg_emotionbox_page_select');
+    	$page = $(this).attr("value");
+        $(".emotionbox_show_ul_inner").empty().load(baseUrl + "other/loademotion" + "?page=" + $page);
+    });
 });
