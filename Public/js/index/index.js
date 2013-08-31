62de37924687409f60e973de3eb7cae5fae073c8
chenkehao <121670155@qq.com>
2013-08-31 11:12:13 +0800

    test shell add

--- Public/js/index/index.js
+++ Public/js/index/index.js
@@ -91,7 +91,7 @@ $().ready(function(){
     /**
      * enter keydown submit
      */
-    $(window).keydown(function(e){
+    $(window).keypress(function(e){
     	alert(e.keyCode);
     	if(e.keyCode == 13) {
     		$('#submit').click();
