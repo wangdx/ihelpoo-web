8119c59404a8389e5d6956b74f1d8626d2222118
chenkehao <121670155@qq.com>
2013-09-11 16:52:15 +0800

    test shell add

--- App/Lib/Action/UserAction.class.php
+++ App/Lib/Action/UserAction.class.php
@@ -707,7 +707,7 @@ class UserAction extends Action {
         	$resultsSchoolInfo = $SchoolInfo->where("status = 1")->order("initial ASC")->select();
         	echo '<div class="setting_school_list_div"><a class="gray f12" id="setting_school_close_span"><span class="close_x" title="关闭">×</span></a><ul>';
         	foreach ($resultsSchoolInfo as $schoolInfo) {
-        		echo "<li><a href='".__ROOT__."/user/register?school=$schoolInfo[id]'>$schoolInfo[school]</a></li>";
+        		echo "<li><span class='gray'>[".strtoupper($schoolInfo['initial'])."]</span> <a href='".__ROOT__."/user/register?school=$schoolInfo[id]'>$schoolInfo[school]</a></li>";
         	}
         	echo '</ul>';
         	echo '<br /><p><a href="/about/sns" class="f12 gray fr">还没你们学校?</a></p><br />';
