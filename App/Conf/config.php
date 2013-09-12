1fa7be5633343ba435f1398c1f1f3fc54f56241b
chenkehao <121670155@qq.com>
2013-09-12 21:22:25 +0800

    test shell add

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -4,12 +4,12 @@ $ooconfig = require 'config_oo.php';
 
 $config = array(
     //'配置项'=>'配置值'
-    'APP_DEBUG'=>true,
-    'SHOW_PAGE_TRACE'=>true,
+    'APP_DEBUG'=>false,
+    'SHOW_PAGE_TRACE'=>false,
 	'URL_CASE_INSENSITIVE'=>true,
     'URL_HTML_SUFFIX'=>'.html',
     'DB_PREFIX'=>'i_',
-	//'TMPL_EXCEPTION_FILE'=>'./App/Tpl/Public/error.html',
+	'TMPL_EXCEPTION_FILE'=>'./App/Tpl/Public/error.html',
 
 	'MAIL_ADDRESS'=>'info@ihelpoo.com', //email
 	'MAIL_SMTP'=>'smtp.exmail.qq.com',
