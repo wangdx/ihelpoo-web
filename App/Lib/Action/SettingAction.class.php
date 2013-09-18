30e855562a533e96adafd9c4984d9fa26fa075d3
chenkehao <121670155@qq.com>
2013-09-18 21:24:17 +0800

    test shell add

--- App/Lib/Action/SettingAction.class.php
+++ App/Lib/Action/SettingAction.class.php
@@ -380,7 +380,6 @@ class SettingAction extends Action
         $this->display();
     }
 
-
     public function icon()
     {
         $this->assign('title', '修改头像');
@@ -521,69 +520,6 @@ class SettingAction extends Action
             );
             $RecordDynamic->add($newRecordDynamicData);
             $this->ajaxReturn($newfilepath, '保存成功', 'ok');
-        }
-
-        if ($this->isPost()) {
-            if (!empty($_FILES)) {
-                if ($_FILES["uploadedimg"]["error"] > 0) {
-                    $this->ajaxReturn(0, '上传图片失败, info' . $_FILES["uploadedimg"]["error"], 'error');
-                } else {
-                    $imageOldName = $_FILES["uploadedimg"]["name"];
-                    $imageType = $_FILES["uploadedimg"]["type"];
-                    $imageType = trim($imageType);
-                    $imageSize = $_FILES["uploadedimg"]["size"];
-                    $imageTmpName = $_FILES["uploadedimg"]["tmp_name"];
-                }
-                if ($imageSize > 3670016) {
-                    $this->ajaxReturn(0, '上传图片太大, 最大能上传单张 3.5MB', 'error');
-                } else if ($imageType == 'image/jpeg' || $imageType == 'image/pjpeg' || $imageType == 'image/gif' || $imageType == 'image/x-png' || $imageType == 'image/png') {
-
-                    /**
-                     * storage in upyun
-                     */
-                    Vendor('Ihelpoo.Upyun');
-                    $upyun = new UpYun('ihelpoo', 'image', 'ihelpoo2013');
-                    $fh = fopen($imageTmpName, 'rb');
-                    $fileName = 'iconorignal' . $userloginid . time() . '.jpg';
-                    $storageTempFilename = '/useralbum/' . $userloginid . '/' . $fileName;
-                    $rsp = $upyun->writeFile($storageTempFilename, $fh, True);
-                    fclose($fh);
-                    $imageStorageUrl = image_storage_url();
-                    $newfilepath = $imageStorageUrl . $storageTempFilename;
-
-                    $opts = array(
-                        UpYun::X_GMKERL_TYPE => 'fix_max',
-                        UpYun::X_GMKERL_VALUE => 150,
-                        UpYun::X_GMKERL_QUALITY => 95,
-                        UpYun::X_GMKERL_UNSHARP => True
-                    );
-                    $fh = fopen($imageTmpName, 'rb');
-                    $storageThumbTempFilename = '/useralbum/' . $userloginid . '/thumb_' . $fileName;
-                    $rsp = $upyun->writeFile($storageThumbTempFilename, $fh, True, $opts);
-                    fclose($fh);
-
-                    /**
-                     * insert into i_user_album
-                     */
-                    $UserAlbum = M("UserAlbum");
-                    $newAlbumIconData = array(
-                        'uid' => $userloginid,
-                        'type' => 1,
-                        'url' => $newfilepath,
-                        'size' => $imageSize,
-                        'time' => time()
-                    );
-                    $UserAlbum->add($newAlbumIconData);
-
-                    /**
-                     * ajax return
-                     */
-                    $this->ajaxReturn($newfilepath, '上传成功', 'uploaded');
-                } else {
-                    $this->ajaxReturn(0, '上传图片格式错误, 目前仅支持.jpg .png .gif', 'error');
-                }
-            }
-            exit();
         }
         $this->display();
     }
--- Public/js/setting/icon.js
+++ Public/js/setting/icon.js
@@ -28,9 +28,46 @@ $().ready(function(){
         });
     }
     
+    $('#file_upload').uploadify({
+    	'formData' : {'userloginid': $("#sessionuserloginid").val()},
+		'fileObjName'      : 'uploadedimg',
+		'fileSizeLimit' : '1000KB',
+		'fileTypeDesc' : 'Image Files',
+        'fileTypeExts' : '*.gif; *.jpg; *.png',
+		'swf'      : '/Public/js/public/uploadify.swf',
+		'uploader' :  baseUrl + '/other/iconupload',
+		'buttonText' : '上传图片',
+		'width' : '90',
+		'height' : '25',
+		'queueSizeLimit' : '1',
+		'onUploadSuccess' : function(file, data, response) {
+			var msg = JSON.parse(data);
+			if (msg.status == 'uploaded') {
+    	        $('#usericontarget').attr({'src': msg.data});
+    	        $('#preview').attr({'src': msg.data});
+    	        $('#img_temp_path').val(msg.data);
+    	        $('.image_upload_div').slideUp('slow');
+    	        $('.image_cut_div').slideDown('fast');
+    	        //icon cut
+    	        jQuery('#usericontarget').Jcrop({
+    	            minSize: [200, 150],
+    	            setSelect: [10, 8, 480, 360],
+    	            bgColor: "#000",
+    	            bgOpacity: 0.6,
+    	            onChange: updatePreview,
+    	            onSelect: updateCoords,
+    	            aspectRatio: 4/3
+    	        });
+    	        $('.icon_handle_info').html('');
+    	        return false;
+    	    } else if (msg.status == 'error') {
+    	        $('.icon_handle_info').fadeIn('fast').html("<span class='f12 red'>" + msg.info + "</span>").delay(1000).fadeOut('fast');
+    	    }
+        }
+	});
+    
     /**
      * new icon from upload
-     */
     $("#icon_upload_btn").click(function(){
         var upload_icon_file = $('#upload_form_icon_file').val();
         if (upload_icon_file == '') {
@@ -68,6 +105,7 @@ $().ready(function(){
             });
         }
     });
+     */
     
     //icon cut submit
     $('#icon_cut_btn').click(function(){
@@ -110,8 +148,8 @@ function updateCoords(c){
 };
 function updatePreview(c){
     if (parseInt(c.w) > 0) {
-        var rx = 500 / c.w;
-        var ry = 375 / c.h;
+        var rx = 230 / c.w;
+        var ry = 172 / c.h;
         $('#preview').css({
             width: Math.round(rx * $('#usericontarget').width()) + 'px',
             height: Math.round(ry * $('#usericontarget').height()) + 'px',
