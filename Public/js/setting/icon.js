38b93c449e539dd8d63da215db8aa0be532983db
chenkehao <121670155@qq.com>
2013-09-18 22:00:35 +0800

    test shell add

--- Public/js/setting/icon.js
+++ Public/js/setting/icon.js
@@ -31,7 +31,7 @@ $().ready(function(){
     $('#file_upload').uploadify({
     	'formData' : {'userloginid': $("#sessionuserloginid").val()},
 		'fileObjName'      : 'uploadedimg',
-		'fileSizeLimit' : '1800KB',
+		'fileSizeLimit' : '3000KB',
 		'fileTypeDesc' : 'Image Files',
         'fileTypeExts' : '*.gif; *.jpg; *.jpeg; *.png',
 		'swf'      : '/Public/js/public/uploadify.swf',
