22c229f09af81342de0ff450868082060c23d258
chenkehao <121670155@qq.com>
2013-09-18 15:17:02 +0800

    test shell add

--- Public/js/setting/iconback.js
+++ Public/js/setting/iconback.js
@@ -33,6 +33,7 @@ $().ready(function(){
 		'swf'      : '/Public/js/public/uploadify.swf',
 		'uploader' : '/ajax/iconupload',
 		'buttonText' : '上传图片',
+		'width' : '100',
 		'onUploadSuccess' : function(file, data, response) {
             $('.icon_handle_info').html('The file ' + file.name + ' was successfully uploaded with a response of ' + response + ':' + data);
         }
