05e71ebad4b2c7bdbcc4ea644a1be44f6969e0e6
chenkehao <121670155@qq.com>
2013-09-26 12:14:00 +0800

    test shell add

--- App/Lib/Action/AboutAction.class.php
+++ App/Lib/Action/AboutAction.class.php
@@ -215,7 +215,7 @@ class AboutAction extends Action {
     	$page = i_page_get_num();
         $count = 25;
         $offset = $page * $count;
-    	$recordDataSuggestion = $DataSuggestion->where("school_id = $recordSchoolInfo[id]")
+    	$recordDataSuggestion = $DataSuggestion->where("i_data_suggestion.school_id = $recordSchoolInfo[id]")
     	->join('i_user_login ON i_data_suggestion.uid = i_user_login.uid')
     	->join('i_school_info ON i_data_suggestion.school_id = i_school_info.id')
         ->field('i_data_suggestion.id,i_user_login.uid,i_data_suggestion.suggestion,i_data_suggestion.time,i_data_suggestion.ihelpoo_reply,i_data_suggestion.ihelpoo_reply_uid,i_data_suggestion.ihelpoo_reply_time,i_data_suggestion.school_reply,i_data_suggestion.school_reply_uid,i_data_suggestion.school_reply_time,i_data_suggestion.school_id,nickname,sex,birthday,enteryear,type,online,active,icon_url,i_school_info.school,i_school_info.domain,i_school_info.domain_main')
