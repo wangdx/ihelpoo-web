046108f82e068bcbf406c4ab943be2d3e9162628
chenkehao <121670155@qq.com>
2013-09-27 15:23:56 +0800

    test shell add

--- Public/js/rooter/suggestion.js
+++ Public/js/rooter/suggestion.js
@@ -20,8 +20,39 @@ $().ready(function(){
             	} else {
             		alert(msg.info);
             	}
-        		
             }
         });
     });
+    
+    $('.deletesuggestion').live('click', function(){
+    	var deleteid = $(this).parent().parent().find('.suggestion_content_div').find('.suggestion_reply_div').attr("suggestionid");
+    	$deleteLi = $(this).parent().parent();
+    	$deleteLi.css("backgroundColor", "#FFFA85");
+    	var infohtml = "<p>确定删除？</p> <a class='btn_sure' id='delete_comment' value='"+deleteid+"'>确定</a><a class='btn_cancel'>取消</a>";
+    	ajaxInfo(infohtml);
+    });
+    
+    $('#delete_comment').live('click', function(){
+    	var deleteid = $(this).attr("value");
+    	$.ajax({
+            type: "POST",
+            url: baseUrl + "rooter/suggestion",
+            data: "suredel=" + deleteid,
+            dataType: "json",
+            success:function(msg){
+            	if (msg.status = 'yes') {
+	            	$deleteLi.slideUp('fast');
+	                $("#ajax_info_div").fadeOut("fast");
+	        		$("#ajax_info_div_outer").hide();
+            	}
+            }
+        });
+    });
+    
+    $('.btn_cancel').live('click', function(){
+    	$("#ajax_info_div").fadeOut("fast");
+		$("#ajax_info_div_outer").hide();
+		$deleteLi.css("backgroundColor", "#FFF");
+    });
+    
 });
