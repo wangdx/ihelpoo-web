fae84337b4dbcf3214aae00a5a7f400842b42753
chenkehao <121670155@qq.com>
2013-09-27 13:03:50 +0800

    test shell add

---
+++ Public/js/rooter/suggestion.js
@@ -0,0 +1,42 @@
+$().ready(function(){
+	
+    $(".reply_show_a").click(function(){
+    	$this = $(this);
+    	$this.parent().parent().find('.suggestion_reply_div').show();
+    	/**
+    	var userid = $(this).attr('uid');
+    	$.ajax({
+            type: "POST",
+            url: baseUrl + "rooter/orderusericon",
+            data:{userid: userid, way: 'up'},
+            dataType: "json",
+            success:function(msg){
+            	if (msg.status == 'yes') {
+            		$this.parent().find(".usericon_ordered_info").append("<span class='f12'><span class='icon_up'></span>" + msg.info + "</span>");
+            	} else {
+            		alert(msg.info);
+            	}
+        		
+            }
+        });*/
+    });
+    
+    $(".usericon_down").click(function(){
+    	$this = $(this);
+    	var userid = $(this).attr('uid');
+    	$.ajax({
+            type: "POST",
+            url: baseUrl + "rooter/orderusericon",
+            data:{userid: userid, way: 'down'},
+            dataType: "json",
+            success:function(msg){
+            	if (msg.status == 'yes') {
+            		$this.parent().find(".usericon_ordered_info").append("<span class='f12'><span class='icon_download'></span>" + msg.info + "</span>");
+            	} else {
+            		alert(msg.info);
+            	}
+        		
+            }
+        });
+    });
+});
