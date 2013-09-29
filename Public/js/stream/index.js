8544e9d0bd40103f70a885b596252b0f55706b2e
chenkehao <121670155@qq.com>
2013-09-29 16:56:58 +0800

    test shell add

--- Public/js/stream/index.js
+++ Public/js/stream/index.js
@@ -919,21 +919,21 @@ $().ready(function(){
 	    	var imageTempContent = $(this).parent().html();
 	    	$(this).parent().val(imageTempContent);
 	    	var $imageobjectpart = $(this).parent().html('<p class="f12 s_li_p_content_image_title"><a href="'+imageurllarge+'" target="_blank"><span class="icon_plus"></span>查看原图</a> <a class="s_li_p_content_image_title_up"><span class="icon_up"></span>收起</a></p><img class="enlargeimg" src="'+baseUrl+'Public/image/common/ajax_wait_login.gif" enlargeswitch="on" title="点击缩小" />');
+	    	var $imageobject = $imageobjectpart.find('.enlargeimg');
+	    	var imagelenlargeheight = $imageobject.height();
+	    	if (imagelenlargeheight > imagelodingmarginheight) {
+	    		$slipContentImagePart.attr({'style': 'min-height:'+imagelodingmarginheight+'px'});
+	    	} else {
+	    		$slipContentImagePart.attr({'style': 'min-height:'+imagelenlargeheight+'px'});
+	    	}
+	    	$imageobject.attr({'src':imageurllarge});
+	    	$imageobject.ready(function(){
+	    		$imageobject.attr({'src':imageurllarge});
+			});
     	} else {
     		var imageTempContentBack = $(this).parent().val();
     		$(this).parent().html(imageTempContentBack);
     	}
-    	var $imageobject = $imageobjectpart.find('.enlargeimg');
-    	var imagelenlargeheight = $imageobject.height();
-    	if (imagelenlargeheight > imagelodingmarginheight) {
-    		$slipContentImagePart.attr({'style': 'min-height:'+imagelodingmarginheight+'px'});
-    	} else {
-    		$slipContentImagePart.attr({'style': 'min-height:'+imagelenlargeheight+'px'});
-    	}
-    	$imageobject.attr({'src':imageurllarge});
-    	$imageobject.ready(function(){
-    		$imageobject.attr({'src':imageurllarge});
-		});
     });
     $('.s_li_p_content_image_title_up').live('click', function(){
     	var imageTempContentBack = $(this).parent().parent().val();
