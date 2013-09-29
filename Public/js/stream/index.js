ee5858e7f593df693dfe6b3dde4ff1f7378a20af
chenkehao <121670155@qq.com>
2013-09-29 16:37:18 +0800

    test shell add

--- Public/js/stream/index.js
+++ Public/js/stream/index.js
@@ -920,7 +920,7 @@ $().ready(function(){
 		    	$(this).parent().val(imageTempContent);
 		    	var $imageobjectpart = $(this).parent().html('<p class="f12 s_li_p_content_image_title"><a href="'+imageurllarge+'" target="_blank"><span class="icon_plus"></span>查看原图</a> <a class="s_li_p_content_image_title_up"><span class="icon_up"></span>收起</a></p><img class="enlargeimg" style="margin-bottom:'+imagelodingmarginheight+'px;" src="'+baseUrl+'Public/image/common/ajax_wait_login.gif" enlargeswitch="on" title="点击缩小" />');
 		    	var $imageobject = $imageobjectpart.find('.enlargeimg');
-		    	$imageobject.attr({'src':imageurllarge});
+		    	$imageobject.attr({'src':imageurllarge, 'style': 'margin-bottom:0px;'});
 		    	$imageobject.ready(function(){
 		    		$imageobject.attr({'src':imageurllarge});
 				});
