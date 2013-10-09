53593dde1d4d859757c592d8638c4875f13e4040
chenkehao <121670155@qq.com>
2013-10-09 14:53:35 +0800

    test shell add

--- Public/js/mobile/message_index.js
+++ Public/js/mobile/message_index.js
@@ -11,7 +11,7 @@ $().ready(function(){
                 var totalmsgnums = parseInt(msg.data.messageSystemNums) + parseInt(msg.data.messageCommentNums) + parseInt(msg.data.messageAtNums) + parseInt(msg.data.messageTalkNums);
                 alert(totalmsgnums);
                 if (msg.data.messageSystemNums != 0) {
-                    $('#message_system').find('.ui-btn-text').append( "<span class='red'>("+msg.data.messageSystemNums+")</span>");
+                    $('#message_system').find('.ui-btn-text').append(" <span class='reb_back'>("+msg.data.messageSystemNums+")</span>");
                 }
                 if (msg.data.messageCommentNums != 0) {
                 	$('#message_comment').append(msg.data.messageSystemNums);
