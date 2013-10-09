6f90c550272342834396569a645908351e367a3e
chenkehao <121670155@qq.com>
2013-10-09 15:13:35 +0800

    test shell add

--- Public/js/mobile/message_index.js
+++ Public/js/mobile/message_index.js
@@ -1,4 +1,10 @@
 $().ready(function(){
+	
+	var message_active_nums = $("#message_active_nums").val();
+	if (message_active_nums != 0) {
+    	$('#message_active').find('.ui-btn-text').append(" <span class='reb_back'>("+message_active_nums+")</span>");
+    }
+	
 	$.ajax({
         type: "POST",
         url: baseUrl + "ajax/getmessage",
