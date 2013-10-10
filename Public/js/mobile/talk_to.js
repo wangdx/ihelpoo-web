f14fc96bd8c78d78a61c56497aaa0701671358d2
chenkehao <121670155@qq.com>
2013-10-10 15:30:05 +0800

    test shell add

--- Public/js/mobile/talk_to.js
+++ Public/js/mobile/talk_to.js
@@ -37,20 +37,19 @@ $().ready(function(){
 
     $("#img_upload_btn").click(function () {
         var upload_image_file = $('#upload_form_img_file').val();
-        var $infoLoading = $('<img/>').attr({'src': baseUrl + 'Public/image/common/progressbar.gif', 'title': '上传中...请稍等'});
         if (upload_image_file == '') {
             $('.imgajaxloading_span').fadeIn('fast').html("<span class='f12 red_l'>还没有选择图片呢</span>").delay(1000).fadeOut('fast');
         } else {
-            $('.imgajaxloading_span').fadeIn('fast').html($infoLoading);
+        	$.mobile.showPageLoadingMsg();
             $.ajaxFileUpload({
                 url: baseUrl + 'ajax/imgtalkupload',
                 secureuri: false,
                 fileElementId: 'upload_form_img_file',
                 dataType: 'json',
                 success: function (msg) {
-                    $infoLoading.remove();
+                	$.mobile.hidePageLoadingMsg();
                     if (msg.status == 'uploaded') {
-                        var uploadImgList = "<li class='upload_img_list' url='" + msg.data + "'><img src='" + msg.data + "' width='80'/><a href='" + msg.data + "' target='_blank' class='f12'><span class='icon_search' title='看大图'></span>大图</a> <a class='re_upload_img'><span class='icon_recycle'></span>重传</a></li>";
+                        var uploadImgList = "<li class='upload_img_list' url='" + msg.data + "'><a href='" + msg.data + "' target='_blank'><img src='" + msg.data + "' width='80'/></a> <a class='re_upload_img'><span class='icon_recycle'></span>重传</a></li>";
                         $('#image_upload_url').val(msg.data);
                         $('#image_upload_list_ul').empty().append(uploadImgList);
                         $('#img_upload_form').hide();
