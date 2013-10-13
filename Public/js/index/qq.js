9810c14cac7555b8f9a3beae73fedf09bfd83f95
chenkehao <121670155@qq.com>
2013-10-13 22:03:38 +0800

    test shell add

--- Public/js/index/qq.js
+++ Public/js/index/qq.js
@@ -14,7 +14,7 @@ $().ready(function(){
 				"i_qq_user_birth_day" : s.data.data.birth_day ,
 				"i_qq_user_birth_month" : s.data.data.birth_month ,
 				"i_qq_user_birth_year" : s.data.data.birth_year ,
-				"i_qq_user_description" : s.data.data.tweetinfo[0].origtext
+				//"i_qq_user_description" : s.data.data.tweetinfo[0].origtext
 			},
 			dataType: 'json',
 			success:function(msg){
