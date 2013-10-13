eff2ed463dcf94372f743c2e269a880ce989111a
chenkehao <121670155@qq.com>
2013-10-13 23:24:04 +0800

    test shell add

--- Public/js/stream/index.js
+++ Public/js/stream/index.js
@@ -716,7 +716,7 @@ $().ready(function(){
     /**
      * diffusion part
      */
-    $('.diffusion').live("toggle",
+    $('.diffusion').toggle(
         function(){
         	$diffusionRecordObj = $(this);
             $(this).parent().parent().find('.diffusion_view_div_box').slideDown('fast');
@@ -730,7 +730,7 @@ $().ready(function(){
         }
     );
     
-    $('.diffusion_view_btn').live("click", function(){
+    $('.diffusion_view_btn').click(function(){
         var $diffusion_view = $(this).parent().find('.diffusion_view_textarea').val();
         if ($diffusion_view == '说点什么吧...') {
         	$diffusion_view = '';
