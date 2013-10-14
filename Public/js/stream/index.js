8f6aeb257bac522665d952a41562293138d9ca0e
chenkehao <121670155@qq.com>
2013-10-14 11:33:43 +0800

    test shell add

--- Public/js/stream/index.js
+++ Public/js/stream/index.js
@@ -1017,8 +1017,13 @@ $().ready(function(){
 		$(".user_info_div").hide();
     	$this = $(this);
     	var pagestring = $this.attr('href');
-    	var pagestringhandle = pagestring.substr(7);
-    	$(".stream_list_div").load(baseUrl + "stream/streamloading" + pagestringhandle);
+    	var iswholedomain = pagestring.match("ihelpoo");
+    	if (iswholedomain == 'ihelpoo') {
+    		$(".stream_list_div").load(pagestring);
+    	} else {
+    		var pagestringhandle = pagestring.substr(7);
+    		$(".stream_list_div").load(baseUrl + "stream/streamloading" + pagestringhandle);
+    	}
     	return false;
     });
 	
