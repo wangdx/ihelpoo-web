bc559f6032d9282c7e4dd971718dabc874808491
chenkehao <121670155@qq.com>
2013-10-16 12:33:42 +0800

    test shell add

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -26,7 +26,7 @@ $config = array(
 	'VERIFI_RECORD_UNMS'=>'3',
 	'VERIFI_COMMENT_UNMS'=>'60',
 	'VERIFI_COMMENT_TIME'=>'8',
-	'IMAGE_STORAGE_URL'=>'http://img.ihelpoo.cn',
+	//'IMAGE_STORAGE_URL'=>'http://img.ihelpoo.cn',
 );
 
 return array_merge($config,$ooconfig);
