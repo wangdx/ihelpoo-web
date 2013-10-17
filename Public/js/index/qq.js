514f78991ec2c1efcc4132e6524ead003ba0a8d4
chenkehao <121670155@qq.com>
2013-10-17 21:19:19 +0800

    test shell add

--- Public/js/index/qq.js
+++ Public/js/index/qq.js
@@ -7,13 +7,12 @@ $().ready(function(){
 		var paras = {};
 		QC.api("get_user_info", paras).success(function(o){
 			QC.api("get_info", paras).success(function(s){
-				alert(o.data.nickname);
-				/*$.ajax({
+				$.ajax({
 					type: "POST",
 					url: baseUrl + "user/loginqqaajax",
 					data: {
 						"i_qq_user_id" : s.data.data.openid ,
-						"i_qq_user_name" : o.nickname ,
+						"i_qq_user_name" : o.data.nickname ,
 						"i_qq_user_sex" : s.data.data.sex ,
 						"i_qq_user_birth_day" : s.data.data.birth_day ,
 						"i_qq_user_birth_month" : s.data.data.birth_month ,
@@ -30,7 +29,7 @@ $().ready(function(){
 							alert(msg.info);
 						}
 					}
-				});*/
+				});
 			})
 			.error(function(f){
 				alert("获取用户信息失败！");
