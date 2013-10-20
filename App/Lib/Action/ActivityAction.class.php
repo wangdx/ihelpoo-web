d0d2dcd02df8b5e1026f4581ef45041fc5200947
chenkehao <121670155@qq.com>
2013-10-20 18:31:16 +0800

    test shell add

--- App/Lib/Action/ActivityAction.class.php
+++ App/Lib/Action/ActivityAction.class.php
@@ -674,11 +674,11 @@ class ActivityAction extends Action {
     	 * partner show
     	 */
     	$isParterActivityUsers = $ActivityUser->where("aid = $recordActivityItem[aid] AND uid = '$userloginid' AND partner_uid !=''")->find();
-    	var_dump($isParterActivityUsers);
-    	$UserLogin = M("UserLogin");
-    	$isHasParterActivityUsers = $UserLogin->find($isParterActivityUsers['partner_uid']);
-    	var_dump($isHasParterActivityUsers);
-    	$this->assign('isHasParterActivityUsers', $isHasParterActivityUsers);
+    	if (!empty($isParterActivityUsers)) {
+	    	$UserLogin = M("UserLogin");
+	    	$isHasParterActivityUsers = $UserLogin->find($isParterActivityUsers['partner_uid']);
+	    	$this->assign('isHasParterActivityUsers', $isHasParterActivityUsers);
+    	}
 
     	/**
     	 * activity info
