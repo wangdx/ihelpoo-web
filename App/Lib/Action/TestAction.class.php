d6ec58bba51aaa12db4c28d54b6ee2e167eaeeec
chenkehao <121670155@qq.com>
2013-10-23 20:43:50 +0800

    test shell add

--- App/Lib/Action/TestAction.class.php
+++ App/Lib/Action/TestAction.class.php
@@ -1,7 +1,5 @@
 <?php
 
-xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
-xhprof_enable();
 /**
  * 本页仅供测试
  */
@@ -9,18 +7,20 @@ class TestAction extends Action {
 
     protected function _initialize() {
         header("Content-Type:text/html; charset=utf-8");
-//        exit();
     }
     
     public function index() {
+    	Vendor('Ihelpoo.Browser');
+    	$browserObj = new Browser();
+    	$getBrowser = $browserObj->getBrowser();
+    	if ($getBrowser == "IE" || $getBrowser == "Android") {
+    		$fromBrowser = $browserObj->getPlatform() . " " . $getBrowser . " " . $browserObj->getVersion();
+    	} else {
+    		$fromBrowser = $browserObj->getPlatform() . " " . $getBrowser;
+    	}
+    	echo $fromBrowser;
     }
 
 }
-$xhprof_data = xhprof_disable();
-include_once "./xhprof_lib/utils/xhprof_lib.php";
-include_once "./xhprof_lib/utils/xhprof_runs.php";
-$xhprof_runs = new XHProfRuns_Default();
-$run_id = $xhprof_runs->save_run($xhprof_data, 'xhprof');
-echo 'http://42.62.50.238/xhprof_html/index.php?run='.$run_id.'&source=xhprof';
 
 ?>
