ae1de4067b2732b9d25698b4efbf2546b3ec5b3d
wangdx <echowdx@gmail.com>
2013-10-27 20:32:12 +0800

    there might be one issue to be cope with

--- App/Lib/Action/StreamAction.class.php
+++ App/Lib/Action/StreamAction.class.php
@@ -1248,8 +1248,8 @@ class StreamAction extends Action
     {
         $time12hour = time() - 43200;
         $userDiffusion12hourAll = $RecordDiffusion->where("uid = $userloginid AND time > $time12hour")->order("time DESC")->count();// FIXME order by?
-        if ($userDiffusion12hourAll >= $threshold) {// FIXME >=?
-            $this->ajaxReturn("", "你扩散了太多消息，休息休息再来吧 :)<br>每12小时最多扩散3条", "0");;
+        if ($userDiffusion12hourAll >= $threshold) {
+            $this->ajaxReturn("", "你扩散了太多消息，休息休息再来吧 :)<br>每12小时最多扩散3条", "0");
             exit();
         }
     }
