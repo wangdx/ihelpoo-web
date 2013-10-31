5548871696f4e0577d5f805a42dd0e857d87375d
chenkehao <121670155@qq.com>
2013-10-31 11:20:19 +0800

    test shell add

--- Public/js/index/index.js
+++ Public/js/index/index.js
@@ -123,6 +123,171 @@ $().ready(function(){
     		$(this).text('微博、QQ登录');
     	}
     });
+    
+    /**
+     * weibo
+     */
+    var islogin = WB2.checkLogin();
+	if (!islogin) {
+	    WB2.anyWhere(function(W){
+	    	W.widget.connectButton({
+	    		id: "wb_connect_btn",
+	    		type:'3,3',
+	    		callback : {
+	    			login:function(o){
+	    			    //登录后的回调函数
+	    			    $.ajax({
+	    			        type: "POST",
+	    			        url: baseUrl + "user/loginweibo",
+	    			        data: {
+	    			        	"i_w_user_id" : o.id ,
+	    			        	"i_w_user_name" : o.name ,
+	    			        	"i_w_user_sex" : o.gender ,
+	    			        	"i_w_user_description" : o.description
+	    			        },
+	    			        dataType: 'json',
+	    			        success:function(msg){
+	    			            if (msg.status == 'ok') {
+									window.location = baseUrl + '/stream';
+								} else if (msg.status == 'step') {
+									alert(msg.info);
+									window.location = baseUrl + msg.data;
+								} else if (msg.status == 'wrong') {
+									alert(msg.info);
+								}
+	    			        }
+	    			    });
+	    			},
+	    			logout:function(){
+	    			    //退出后的回调函数
+	    				window.location = baseUrl + 'user/quit';
+	    			}
+	    		}
+	    	});
+	    });
+	} else {
+		WB2.anyWhere(function(W){
+	    	W.widget.connectButton({
+	    		id: "wb_connect_btn",
+	    		type:'3,3',
+	    		callback : {
+	    			login:function(o){
+	    				$.ajax({
+	    			        type: "POST",
+	    			        url: baseUrl + "user/loginweibo",
+	    			        data: {
+	    			        	"i_w_user_id" : o.id ,
+	    			        	"i_w_user_name" : o.name ,
+	    			        	"i_w_user_sex" : o.gender ,
+	    			        	"i_w_user_description" : o.description
+	    			        },
+	    			        dataType: 'json',
+	    			        success:function(msg){
+	    			            if (msg.status == 'step') {
+									alert(msg.info);
+								} else if (msg.status == 'wrong') {
+									alert(msg.info);
+								}
+	    			        }
+	    			    });
+	    				$(".loginbox_email_bg").hide();
+	    				$(".loginbox_password_bg").hide();
+	    				$("#qqLoginBtn").hide();
+	    				$(".loginbox_more").hide();
+	    				$(".loginbox_select").html("你已经通过微博登录，<a href='/stream' class='f14'>进入首页</a>");
+	    				$(".loginbox_submit").slideUp('fast');
+	    				$(".loginbox_weibo_qq").slideDown('fast');
+	    			},
+	    			logout:function(){
+	    			    //退出后的回调函数
+	    				window.location = baseUrl + 'user/quit';
+	    			}
+	    		}
+	    	});
+	    });
+	}
+	
+	/**
+	 * qq
+	 */
+	var isqqlogin = QC.Login.check();
+	if (!isqqlogin) {
+		QC.Login({
+			btnId:"qqLoginBtn" 
+		});
+		var paras = {};
+		QC.api("get_user_info", paras).success(function(o){
+			QC.api("get_info", paras).success(function(s){
+				$.ajax({
+					type: "POST",
+					url: baseUrl + "user/loginqqaajax",
+					data: {
+						"i_qq_user_id" : s.data.data.openid ,
+						"i_qq_user_name" : o.data.nickname ,
+						"i_qq_user_sex" : s.data.data.sex ,
+						"i_qq_user_birth_day" : s.data.data.birth_day ,
+						"i_qq_user_birth_month" : s.data.data.birth_month ,
+						"i_qq_user_birth_year" : s.data.data.birth_year
+					},
+					dataType: 'json',
+					success:function(msg){
+						if (msg.status == 'ok') {
+							window.location = baseUrl + msg.data;
+						} else if (msg.status == 'step') {
+							alert(msg.info);
+							window.location = baseUrl + msg.data;
+						} else if (msg.status == 'wrong') {
+							alert(msg.info);
+						}
+					}
+				});
+			})
+			.error(function(f){
+				alert("获取用户信息失败！");
+			});
+		})
+		.error(function(f){
+			alert("获取用户信息失败！");
+		});
+	} else {
+		QC.Login({
+			btnId:"qqLoginBtn" 
+		});
+		var paras = {};
+		QC.api("get_info", paras).success(function(s){
+			$.ajax({
+				type: "POST",
+				url: baseUrl + "user/loginqqaajax",
+				data: {
+					"i_qq_user_id" : s.data.data.openid ,
+					"i_qq_user_name" : s.data.data.nickname ,
+					"i_qq_user_sex" : s.data.data.sex ,
+					"i_qq_user_birth_day" : s.data.data.birth_day ,
+					"i_qq_user_birth_month" : s.data.data.birth_month ,
+					"i_qq_user_birth_year" : s.data.data.birth_year
+				},
+				dataType: 'json',
+				success:function(msg){
+					if (msg.status == 'step') {
+						alert(msg.info);
+					} else if (msg.status == 'wrong') {
+						alert(msg.info);
+					}
+					$(".loginbox_email_bg").hide();
+    				$(".loginbox_password_bg").hide();
+    				$("#wb_connect_btn").hide();
+    				$("#qqLoginBtn").css({'left':'0px'});
+    				$(".loginbox_more").hide();
+    				$(".loginbox_select").html("你已经通过QQ登录，<a href='/stream' class='f14'>进入首页</a>");
+    				$(".loginbox_submit").slideUp('fast');
+    				$(".loginbox_weibo_qq").slideDown('fast');
+				}
+			});
+		})
+		.error(function(f){
+			alert("获取用户信息失败！");
+		});
+	}
 });
 
 function loadBackground()
