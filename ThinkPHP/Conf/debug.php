e31be824446924dd2e9cfe5866835028d1755264
chenkehao <121670155@qq.com>
2013-11-08 15:09:28 +0800

    test shell add

--- ThinkPHP/Conf/debug.php
+++ ThinkPHP/Conf/debug.php
@@ -22,7 +22,7 @@ defined('THINK_PATH') or exit();
 return  array(
     'LOG_RECORD'			=>	true,  // 进行日志记录
     'LOG_EXCEPTION_RECORD'  => 	true,    // 是否记录异常信息日志
-    'LOG_LEVEL'       		=>  'EMERG,ALERT,CRIT,ERR',  // 允许记录的日志级别
+    'LOG_LEVEL'       		=>  'EMERG,ALERT,CRIT,ERR,WARN',  // 允许记录的日志级别,NOTIC,INFO,DEBUG,SQL
     'DB_FIELDS_CACHE'		=> 	false, // 字段缓存信息
     'DB_SQL_LOG'			=>	true, // 记录SQL信息
     'APP_FILE_CASE'  		=>  true, // 是否检查文件的大小写 对Windows平台有效
