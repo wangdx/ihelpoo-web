cc1efd702854c097e55ce55d4777ebc36a2e57d8
chenkehao <121670155@qq.com>
2013-11-08 15:12:35 +0800

    test shell add

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -4,7 +4,7 @@ $ooconfig = require 'config_oo.php';
 
 $config = array(
     //'配置项'=>'配置值'
-    //'APP_DEBUG'=>false,
+    'APP_DEBUG'=>false,
     'SHOW_PAGE_TRACE'=>false,
 	'URL_CASE_INSENSITIVE'=>true,
     'URL_HTML_SUFFIX'=>'.html',
