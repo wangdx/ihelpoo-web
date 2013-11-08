9ae1f72ca386e6d8a2e004b757281d9f70d67cb5
chenkehao <121670155@qq.com>
2013-11-08 22:34:06 +0800

    test shell add

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 define('THINK_PATH','./ThinkPHP/');
 define('APP_NAME','App');
 define('APP_PATH','./App/');
-//define('APP_DEBUG',true);
+define('APP_DEBUG',true);
 //define('SAE_RUNTIME',true);
 require THINK_PATH.'Extend/Engine/Sae.php';
